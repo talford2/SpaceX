932946edc586f29935c9bb4a9d58e50349cd8947
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-06 00:46:19 +0800

    Remove shiftwbles from list on destroy.

--- Assets/Scripts/Shiftable.cs
+++ Assets/Scripts/Shiftable.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Shiftable : MonoBehaviour
 {
-	[Tooltip("Cell the object is contained in")]
-	public CellIndex UniverseCellIndex;
+    [Tooltip("Cell the object is contained in")]
+    public CellIndex UniverseCellIndex;
 
-	void Awake()
-	{
-		Universe.ShiftableItems.Add(this);
-	}
+    private void Awake()
+    {
+        Universe.ShiftableItems.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        Universe.ShiftableItems.Remove(this);
+    }
 }
