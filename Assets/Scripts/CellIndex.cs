3a68283a65d117bfd311ccd3ef3e12c0313e2f81
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-06 00:15:45 +0800

    Multiply by float scalar operator for CellIndex.

--- Assets/Scripts/CellIndex.cs
+++ Assets/Scripts/CellIndex.cs
@@ -24,6 +24,7 @@ public class CellIndex
 	{
 		return string.Format("({0},{1},{2})", X, Y, Z);
 	}
+
     public static CellIndex operator +(CellIndex c1, CellIndex c2)
     {
         return new CellIndex(c1.X + c2.X, c1.Y + c2.Y, c1.Z + c2.Z);
@@ -32,5 +33,10 @@ public class CellIndex
     public static CellIndex operator -(CellIndex c1, CellIndex c2)
     {
         return new CellIndex(c1.X - c2.X, c1.Y - c2.Y, c1.Z - c2.Z);
+    }
+
+    public static CellIndex operator *(float s, CellIndex c1)
+    {
+        return new CellIndex(Mathf.CeilToInt(s * c1.X), Mathf.CeilToInt(s * c1.Y), Mathf.CeilToInt(s * c1.Z));
     }
 }
