600984044193a20369dfbb73618179a64daa80a1
talford2 <talford2@hotmail.com>
2015-11-07 01:54:21 +1100

    controls

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -2,59 +2,76 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public Vehicle VehiclePrefab;
+	public Vehicle VehiclePrefab;
 
-    private Vehicle _playVehicleInstance;
+	private Vehicle _playVehicleInstance;
 
-    public bool InvertY = false;
+	public bool InvertY = false;
 
-    private void Awake()
-    {
-        _playVehicleInstance = Instantiate<Vehicle>(VehiclePrefab);
-        _current = this;
-    }
+	private void Awake()
+	{
+		_playVehicleInstance = Instantiate<Vehicle>(VehiclePrefab);
+		_current = this;
+	}
 
-    private void Start()
-    {
-        FollowCamera.Current.Target = _playVehicleInstance.transform;
-    }
+	private void Start()
+	{
+		FollowCamera.Current.Target = _playVehicleInstance.transform;
+	}
 
-    private void Update()
-    {
-        if (InvertY)
-        {
-            _playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical")*-1;
-        }
-        else
-        {
-            _playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical");
-        }
-        _playVehicleInstance.YawThrottle = Input.GetAxis("Horizontal");
+	private void Update()
+	{
+		if (InvertY)
+		{
+			_playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical") * -1;
+		}
+		else
+		{
+			_playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical");
+		}
+		_playVehicleInstance.YawThrottle = Input.GetAxis("Horizontal");
 
-        //_playVehicleInstance.CurrentWeapon.IsTriggered = true;
-        _playVehicleInstance.CurrentWeapon.IsTriggered = Input.GetAxis("FireTrigger") > 0;
-        //Debug.Log(_playVehicleInstance.CurrentWeapon.IsTriggered);
-        //if (Input.GetAxis("FireTrigger") != 0)
-        //{
-        //	Debug.LogFormat("FireTrigger 1 : {0}", Input.GetAxis("FireTrigger"));
-        //	_playVehicleInstance.CurrentWeapon.IsTriggered = true;
-        //}
+		//_playVehicleInstance.CurrentWeapon.IsTriggered = true;
+		_playVehicleInstance.CurrentWeapon.IsTriggered = Input.GetAxis("FireTrigger") > 0;
+		//Debug.Log(_playVehicleInstance.CurrentWeapon.IsTriggered);
+		//if (Input.GetAxis("FireTrigger") != 0)
+		//{
+		//	Debug.LogFormat("FireTrigger 1 : {0}", Input.GetAxis("FireTrigger"));
+		//	_playVehicleInstance.CurrentWeapon.IsTriggered = true;
+		//}
+
+		var roll = Input.GetAxis("Roll");
+		Debug.Log("roll = " + roll);
 
 
-        //var vehicle
+		_playVehicleInstance.IsAccelerating = false;
+		if (Input.GetButton("Boost"))
+		{
+			Debug.Log("Boost!");
+			_playVehicleInstance.IsAccelerating = true;
+		}
 
-        // Check for shifting
-    }
+		_playVehicleInstance.IsBraking = false;
+		if (Input.GetButton("Brake"))
+		{
+			Debug.Log("Brake!");
+			_playVehicleInstance.IsBraking = true;
+		}
 
-    private static PlayerController _current;
+		//var vehicle
 
-    public static PlayerController Current
-    {
-        get { return _current; }
-    }
+		// Check for shifting
+	}
 
-    public Vehicle VehicleInstance
-    {
-        get { return _playVehicleInstance; }
-    }
+	private static PlayerController _current;
+
+	public static PlayerController Current
+	{
+		get { return _current; }
+	}
+
+	public Vehicle VehicleInstance
+	{
+		get { return _playVehicleInstance; }
+	}
 }
