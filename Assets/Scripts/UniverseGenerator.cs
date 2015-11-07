f915e25be09470e7d159ad8f3e7a2fae5b369f39
talford2 <talford2@hotmail.com>
2015-11-08 00:03:33 +1100

    Random asteroid field

---
+++ Assets/Scripts/UniverseGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class UniverseGenerator : MonoBehaviour
+{
+	public GameObject AsteroidPrefab;
+
+	public int ObjectCount = 200;
+
+	public float UniverseSize = 1000f;
+	
+	void Start()
+	{
+		for (var i = 0; i < ObjectCount; i++)
+		{
+			var inst = Instantiate(AsteroidPrefab);
+			inst.transform.position = GetRandomPosition();
+			inst.transform.rotation = Random.rotation;
+		}
+	}
+
+	private Vector3 GetRandomPosition()
+	{
+		return Random.insideUnitSphere * UniverseSize;
+	}
+}
