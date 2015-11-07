21eed848cf28066605d13ed9014ded354b1cff69
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-07 19:18:39 +0800

    Create ShiftSwitch.

--- Assets/Scripts/IdleRotation.cs
+++ Assets/Scripts/IdleRotation.cs
@@ -4,22 +4,17 @@ public class IdleRotation : MonoBehaviour
 {
     public float RotationSpeed;
 
-    private Shiftable _shiftable;
     private Vector3 _rotationVelocity;
 
     private void Awake()
     {
         transform.rotation *= Quaternion.Euler(RandomEuler(360f));
-        _shiftable = GetComponent<Shiftable>();
         _rotationVelocity = RandomEuler(RotationSpeed);
     }
 
     private void Update()
     {
-        if (PlayerController.Current.InPlayerActiveCells(_shiftable.UniverseCellIndex))
-        {
-            transform.rotation *= Quaternion.Euler(_rotationVelocity * Time.deltaTime);
-        }
+        transform.rotation *= Quaternion.Euler(_rotationVelocity*Time.deltaTime);
     }
 
     private Vector3 RandomEuler(float magnitude)
---
+++ Assets/Scripts/ShiftSwitch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Shiftable))]
+public class ShiftSwitch : MonoBehaviour
+{
+    public MonoBehaviour SwitchComponent;
+
+    private Shiftable _shiftable;
+
+    private void Awake()
+    {
+        _shiftable = GetComponent<Shiftable>();
+        _shiftable.OnShift += Switch;
+    }
+
+    private void Switch(CellIndex delta)
+    {
+        SwitchComponent.enabled = PlayerController.Current.InPlayerActiveCells(_shiftable.UniverseCellIndex);
+    }
+}
