a234bfbec80337bf8ddb0dc7b82f3957725284f3
talford2 <talford2@hotmail.com>
2015-11-08 02:13:06 +1100

    Lod experiment

--- Assets/Scripts/LevelOfDetail.cs
+++ Assets/Scripts/LevelOfDetail.cs
@@ -6,28 +6,38 @@ public class LevelOfDetail : MonoBehaviour
 {
 	public List<DistanceConfig> Distances;
 
+	public float Interval = 1f;
+	private float _coolDown = 0f;
+
 	private void Awake()
-    {
+	{
+		_coolDown = UnityEngine.Random.Range(0, Interval);
 	}
 
-    private void Update()
-    {
-        var toCamera = transform.position - Camera.main.transform.position;
-        foreach (var distConfig in Distances)
-        {
-            if (toCamera.sqrMagnitude > distConfig.Distance*distConfig.Distance)
-            {
-                foreach (var enableObj in distConfig.Enabled)
-                {
-                    enableObj.SetActive(true);
-                }
-                foreach (var disableObj in distConfig.Disabled)
-                {
-                    disableObj.SetActive(false);
-                }
-            }
-        }
-    }
+	private void Update()
+	{
+		_coolDown += Time.deltaTime;
+
+		if (_coolDown < 0)
+		{
+			var toCamera = transform.position - Camera.main.transform.position;
+			foreach (var distConfig in Distances)
+			{
+				if (toCamera.sqrMagnitude > distConfig.Distance * distConfig.Distance)
+				{
+					foreach (var enableObj in distConfig.Enabled)
+					{
+						enableObj.SetActive(true);
+					}
+					foreach (var disableObj in distConfig.Disabled)
+					{
+						disableObj.SetActive(false);
+					}
+				}
+			}
+			_coolDown = Interval;
+		}
+	}
 }
 
 [Serializable]
---
+++ Assets/Scripts/LevelOfDetail2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelOfDetail2 : MonoBehaviour
+{
+	public float Interval = 1f;
+	public List<DistanceConfig> Distances;
+
+	private void Awake()
+	{
+		StartCoroutine(UpdateLod(Random.Range(0f, Interval)));
+	}
+
+	private IEnumerator UpdateLod(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		var toCamera = transform.position - Camera.main.transform.position;
+		foreach (var distConfig in Distances)
+		{
+			if (toCamera.sqrMagnitude > distConfig.Distance * distConfig.Distance)
+			{
+				foreach (var enableObj in distConfig.Enabled)
+				{
+					enableObj.SetActive(true);
+				}
+				foreach (var disableObj in distConfig.Disabled)
+				{
+					disableObj.SetActive(false);
+				}
+			}
+		}
+		StartCoroutine(UpdateLod(Interval));
+	}
+}
+
+//[Serializable]
+//public class DistanceConfig
+//{
+//	public float Distance;
+
+//	public List<GameObject> Enabled;
+
+//	public List<GameObject> Disabled;
+//}
