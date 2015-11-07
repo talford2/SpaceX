e593653438b3c3d9a6567abb1f03cbf01aa7ad92
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-07 18:20:20 +0800

    Add asteroids.

---
+++ Assets/Scripts/IdleRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class IdleRotation : MonoBehaviour
+{
+    public float RotationSpeed;
+
+    private Vector3 rotationVelocity;
+
+    private void Awake()
+    {
+        transform.rotation *= Quaternion.Euler(RandomEuler(360f));
+        rotationVelocity = RandomEuler(RotationSpeed);
+    }
+
+    private void Update()
+    {
+        transform.rotation *= Quaternion.Euler(rotationVelocity*Time.deltaTime);
+    }
+
+    private Vector3 RandomEuler(float magnitude)
+    {
+        var halfMagnitude = magnitude/2f;
+        return new Vector3(Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude));
+    }
+}
