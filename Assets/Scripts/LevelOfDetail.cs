7638b9db5c8a3b4eb567458aea80632c9a96c4b9
talford2 <talford2@hotmail.com>
2015-11-08 01:44:54 +1100

    LOD stuff

--- Assets/Scripts/LevelOfDetail.cs
+++ Assets/Scripts/LevelOfDetail.cs
@@ -1,37 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelOfDetail : MonoBehaviour
 {
-    public float Distance;
-    public List<MonoBehaviour> SwitchOn;
-    public List<MonoBehaviour> SwitchOff;
+	public List<DistanceConfig> Distances;
 
-    private float distSquared;
+	//public float Distance;
+	//public List<MonoBehaviour> SwitchOn;
+	//public List<MonoBehaviour> SwitchOff;
 
-    private void Awake()
-    {
-        distSquared = Distance*Distance;
-    }
+	//private float distSquared;
 
-    private void Update()
-    {
-        var toCamera = transform.position - Camera.main.transform.position;
-        if (toCamera.sqrMagnitude > distSquared)
-        {
-            Switch();
-        }
-    }
+	//private void Awake()
+	//{
+	//	distSquared = Distance * Distance;
+	//}
 
-    private void Switch()
-    {
-        foreach (var onComponent in SwitchOn)
-        {
-            onComponent.enabled = true;
-        }
-        foreach (var offComponent in SwitchOff)
-        {
-            offComponent.enabled = false;
-        }
-    }
+	//private void Update()
+	//{
+	//	var toCamera = transform.position - Camera.main.transform.position;
+	//	if (toCamera.sqrMagnitude > distSquared)
+	//	{
+	//		Switch();
+	//	}
+	//}
+
+	//private void Switch()
+	//{
+	//	foreach (var onComponent in SwitchOn)
+	//	{
+	//		onComponent.enabled = true;
+	//	}
+	//	foreach (var offComponent in SwitchOff)
+	//	{
+	//		offComponent.enabled = false;
+	//	}
+	//}
+}
+
+[Serializable]
+public class DistanceConfig
+{
+	public float Distance;
+
+	public List<GameObject> Enabled;
+
+	public List<GameObject> Disabled;
 }
