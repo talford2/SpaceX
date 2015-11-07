e6bfd09be0e923a564df855974b92767d17d828c
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-07 19:43:54 +0800

    Add Killable component.

---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Killable : MonoBehaviour
+{
+    public float Health;
+    public float MaxHealth;
+
+    public delegate void OnDamageEvent();
+    public event OnDamageEvent OnDamage;
+
+    public delegate void OnDieEvent();
+    public event OnDieEvent OnDie;
+
+    private bool isAlive;
+
+    private void Awake()
+    {
+        isAlive = true;
+    }
+
+    public void Damage(float damage)
+    {
+        if (isAlive)
+        {
+            Health -= damage;
+            if (OnDamage != null)
+                OnDamage();
+            if (Health < 0f)
+                Die();
+        }
+    }
+
+    public void Die()
+    {
+        isAlive = false;
+        if (OnDie != null)
+            OnDie();
+    }
+}
