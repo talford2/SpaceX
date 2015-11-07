06a6ac0f6cc7aeb37e400f6b4e653463f8e008ac
talford2 <talford2@hotmail.com>
2015-11-08 02:57:17 +1100

    Improving rotation script

--- Assets/Scripts/IdleRotation.cs
+++ Assets/Scripts/IdleRotation.cs
@@ -2,24 +2,31 @@
 
 public class IdleRotation : MonoBehaviour
 {
-    public float RotationSpeed;
+	public float RotationSpeed;
 
-    private Vector3 _rotationVelocity;
+	private Vector3 _rotationVelocity;
 
-    private void Awake()
-    {
-        transform.rotation *= Quaternion.Euler(RandomEuler(360f));
-        _rotationVelocity = RandomEuler(RotationSpeed);
-    }
+	public Transform RotateTransform;
 
-    private void Update()
-    {
-        transform.rotation *= Quaternion.Euler(_rotationVelocity*Time.deltaTime);
-    }
+	private void Awake()
+	{
+		if (RotateTransform == null)
+		{
+			RotateTransform = transform;
+		}
 
-    private Vector3 RandomEuler(float magnitude)
-    {
-        var halfMagnitude = magnitude/2f;
-        return new Vector3(Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude));
-    }
+		RotateTransform.rotation *= Quaternion.Euler(RandomEuler(360f));
+		_rotationVelocity = RandomEuler(RotationSpeed);
+	}
+
+	private void Update()
+	{
+		RotateTransform.rotation *= Quaternion.Euler(_rotationVelocity * Time.deltaTime);
+	}
+
+	private Vector3 RandomEuler(float magnitude)
+	{
+		var halfMagnitude = magnitude / 2f;
+		return new Vector3(Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude), Random.Range(-halfMagnitude, halfMagnitude));
+	}
 }
