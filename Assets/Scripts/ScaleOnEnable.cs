19bc76211049db85b5f0f7d20866220c313f6aa8
talford2 <talford2@hotmail.com>
2015-11-08 19:36:52 +1100

    Scale enable thing

---
+++ Assets/Scripts/ScaleOnEnable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleOnEnable : MonoBehaviour
+{
+	public float AppearTime = 1f;
+	float _coolDown = 0f;
+
+	void Update()
+	{
+		if (_coolDown < AppearTime)
+		{
+			_coolDown += Time.deltaTime;
+			_coolDown = Mathf.Min(_coolDown, AppearTime);
+
+			transform.localScale = Vector3.one * (_coolDown / AppearTime);
+		}
+	}
+
+	void OnEnable()
+	{
+		transform.localScale = Vector3.zero;
+		_coolDown = 0f;
+	}
+}
