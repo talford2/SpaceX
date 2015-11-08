06b1ab94e9f69aa266655d73008db37aa4787808
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-08 16:02:00 +0800

    Deactivate/Activate missiles.

--- Assets/Weapons/Missile.cs
+++ Assets/Weapons/Missile.cs
@@ -2,37 +2,39 @@
 
 public class Missile : MonoBehaviour
 {
-	private LineRenderer _lineRenderer;
-
-	private Shiftable _shiftable;
-
 	public float MissileLength = 6f;
-
 	public float MissileSpeed = 150f;
-
     public float MissileDamage = 5f;
+
+    private bool _isLive;
+    private LineRenderer _lineRenderer;
+    private Shiftable _shiftable;
 
 	void Awake()
 	{
 		_lineRenderer = GetComponent<LineRenderer>();
 		_shiftable = GetComponent<Shiftable>();
+        Stop();
 	}
 
 	public void Update()
 	{
-	    var missileRay = new Ray(transform.position, transform.forward);
-        var displacement = MissileSpeed * Time.deltaTime;
-	    RaycastHit missileHit;
-	    if (Physics.Raycast(missileRay, out missileHit, displacement))
+	    if (_isLive)
 	    {
-	        var killable = missileHit.collider.GetComponentInParent<Killable>();
-	        if (killable != null)
+	        var missileRay = new Ray(transform.position, transform.forward);
+	        var displacement = MissileSpeed*Time.deltaTime;
+	        RaycastHit missileHit;
+	        if (Physics.Raycast(missileRay, out missileHit, displacement))
 	        {
-	            killable.Damage(MissileDamage);
-	            _lineRenderer.enabled = false;
+	            var killable = missileHit.collider.GetComponentInParent<Killable>();
+	            if (killable != null)
+	            {
+	                killable.Damage(MissileDamage);
+	                Stop();
+	            }
 	        }
+	        transform.position += transform.forward*displacement;
 	    }
-        transform.position += transform.forward * displacement;
 	}
 
 	public void LateUpdate()
@@ -45,4 +47,16 @@ public class Missile : MonoBehaviour
 		_lineRenderer.SetPosition(0, transform.position);
 		_lineRenderer.SetPosition(1, transform.position + transform.forward * MissileLength);
 	}
+
+    public void Stop()
+    {
+        _isLive = false;
+        _lineRenderer.enabled = false;
+    }
+
+    public void Shoot()
+    {
+        _isLive = true;
+        _lineRenderer.enabled = true;
+    }
 }
