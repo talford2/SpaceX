352821e61b7fe81c2d828e2b4a2257eb54541b76
talford2 <talford2@hotmail.com>
2015-11-10 02:02:25 +1100

    Space dust

---
+++ Assets/Environment/Billboard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	public Camera UseCamera;
+	public bool Flip;
+
+	public bool UseCameraRotation;
+
+	void Start()
+	{
+		if (UseCamera == null)
+			UseCamera = Camera.main;
+
+		if (Flip)
+		{
+			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * -1);
+		}
+	}
+
+	void Update()
+	{
+		if (UseCameraRotation)
+		{
+			transform.rotation = UseCamera.transform.rotation;
+			transform.Rotate(180, 0, 180, Space.Self);
+		}
+		else
+		{
+			transform.LookAt(UseCamera.transform.position, transform.up);
+			//transform.rotation = Quaternion.LookRotation(Camera.main.transform.up, forward);
+		}
+	}
+}
---
+++ Assets/Environment/SpaceDust.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpaceDust : MonoBehaviour
+{
+	public GameObject DustParticlePrefab;
+
+	private List<GameObject> _particles;
+
+	public Transform CentreTransform;
+
+	public int DustParticleCount = 100;
+
+	public float Radius = 200f;
+	private float _radiusSqr;
+
+	public void Start()
+	{
+		_radiusSqr = Radius * Radius;
+		_particles = new List<GameObject>();
+
+		for (var i = 0; i < DustParticleCount; i++)
+		{
+			var inst = Instantiate(DustParticlePrefab);
+
+			inst.transform.position = Random.insideUnitSphere * Radius;
+			inst.transform.rotation = Random.rotation;
+
+			_particles.Add(inst);
+		}
+
+		CentreTransform = PlayerController.Current.VehicleInstance.transform;
+	}
+
+	public void Update()
+	{
+		foreach (var dustParticle in _particles)
+		{
+			//var sqrMag = (transform.position - CentreTransform.position).sqrMagnitude;
+			var sqrMag = (CentreTransform.position - dustParticle.transform.position).sqrMagnitude;
+
+			if (sqrMag > _radiusSqr)
+			{
+				dustParticle.transform.position = Random.onUnitSphere * Radius + CentreTransform.position;
+				dustParticle.transform.rotation = Random.rotation;
+			}
+			dustParticle.transform.LookAt(CentreTransform);
+		}
+	}
+}
