73618d04d4b31ad4776b757939c5f6589b5ce748
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-09 21:24:09 +0800

    Tidy up shiftable.

--- Assets/Scripts/Shiftable.cs
+++ Assets/Scripts/Shiftable.cs
@@ -34,8 +34,6 @@ public class Shiftable : MonoBehaviour
 		transform.position += translation;
 	}
 
-	private Vector3 lastPosition;
-
 	private void Awake()
 	{
 		Universe.ShiftableItems.Add(this);
@@ -47,22 +45,6 @@ public class Shiftable : MonoBehaviour
 		return new CellIndex(Mathf.CeilToInt(cellZero.x), Mathf.CeilToInt(cellZero.y), Mathf.CeilToInt(cellZero.z));
 	}
 
-	//public void UpdateCell()
-	//{
-	//	var curCell = CellIndexFromPosition(transform.position);
-	//	var lastCell = CellIndexFromPosition(lastPosition);
-	//	var deltaCell = curCell - lastCell;
-
-	//	if (!deltaCell.IsZero())
-	//	{
-	//		UniverseCellIndex += deltaCell;
-	//		if (OnCellIndexChange != null)
-	//			OnCellIndexChange(deltaCell);
-	//	}
-
-	//	lastPosition = transform.position;
-	//}
-
 	private void OnDestroy()
 	{
 		Universe.ShiftableItems.Remove(this);
@@ -70,10 +52,7 @@ public class Shiftable : MonoBehaviour
 
 	public void Shift(Vector3 shiftAmount)
 	{
-		var diff = lastPosition - transform.position;
-
 		transform.position -= shiftAmount;
-		lastPosition = transform.position + diff;
 
 		if (OnShift != null)
 			OnShift(shiftAmount);
