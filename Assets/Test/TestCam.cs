2d744dee7975b3d1f9fe5764579baeacdc0cbb94
talford2 <talford2@hotmail.com>
2015-11-15 21:45:29 +1100

    Particle system shift issue

---
+++ Assets/Test/TestCam.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCam : MonoBehaviour
+{
+	public float Speed = 2f;
+
+	public float FlickBackDistance = 10;
+
+	public GameObject ParticleSystemTest;
+
+	public bool UseLateUpdateForParticles = false;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (!UseLateUpdateForParticles)
+		{
+			MoveParticleSystem();
+		}
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		if (UseLateUpdateForParticles)
+		{
+			MoveParticleSystem();
+		}
+
+		transform.position += Vector3.right * Speed * Time.deltaTime;
+		if (transform.position.x > FlickBackDistance)
+		{
+			transform.position -= Vector3.right * FlickBackDistance;
+		}
+	}
+
+	private void MoveParticleSystem()
+	{
+		ParticleSystemTest.transform.position += Vector3.right * Speed * Time.deltaTime;
+		if (ParticleSystemTest.transform.position.x > FlickBackDistance)
+		{
+			ParticleSystemTest.transform.position -= Vector3.right * FlickBackDistance;
+		}
+	}
+}
