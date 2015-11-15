2644ef3a96319dd0697ff59fe2051c51c2dd2434
talford2 <talford2@hotmail.com>
2015-11-16 01:50:55 +1100

    Some minor improvements

--- Assets/Scripts/Killable.cs
+++ Assets/Scripts/Killable.cs
@@ -2,46 +2,53 @@
 
 public class Killable : MonoBehaviour
 {
-    public float Health;
-    public float MaxHealth;
+	public float Health;
+	public float MaxHealth;
 
-    public GameObject DamageEffect;
-    public GameObject DieEffect;
+	public GameObject DamageEffect;
+	public GameObject DieEffect;
 
-    public delegate void OnDamageEvent(Vector3 position, Vector3 normal);
-    public event OnDamageEvent OnDamage;
+	public delegate void OnDamageEvent(Vector3 position, Vector3 normal);
+	public event OnDamageEvent OnDamage;
 
-    public delegate void OnDieEvent();
-    public event OnDieEvent OnDie;
+	public delegate void OnDieEvent(Killable sender);
+	public event OnDieEvent OnDie;
 
-    private bool isAlive;
+	public bool DestroyOnDie = true;
 
-    private void Awake()
-    {
-        isAlive = true;
-    }
+	private bool isAlive;
 
-    public void Damage(float damage, Vector3 position, Vector3 normal)
-    {
-        if (isAlive)
-        {
-            Health -= damage;
-            if (OnDamage != null)
-                OnDamage(position, normal);
-            if (DamageEffect != null)
-                Instantiate(DamageEffect, position, Quaternion.LookRotation(normal));
-            if (Health < 0f)
-                Die();
-        }
-    }
+	private void Awake()
+	{
+		isAlive = true;
+	}
 
-    public void Die()
-    {
-        isAlive = false;
-        if (OnDie != null)
-            OnDie();
-        if (DieEffect != null)
-            Instantiate(DieEffect, transform.position, transform.rotation);
-        Destroy(gameObject);
-    }
+	public void Damage(float damage, Vector3 position, Vector3 normal)
+	{
+		if (isAlive)
+		{
+			Health -= damage;
+			if (OnDamage != null)
+				OnDamage(position, normal);
+			if (DamageEffect != null)
+				Instantiate(DamageEffect, position, Quaternion.LookRotation(normal));
+			if (Health < 0f)
+				Die();
+		}
+	}
+
+	public void Die()
+	{
+		if (DieEffect != null)
+			Instantiate(DieEffect, transform.position, transform.rotation);
+
+		isAlive = false;
+		if (OnDie != null)
+			OnDie(this);
+
+		if (DestroyOnDie)
+		{
+			Destroy(gameObject);
+		}
+	}
 }
