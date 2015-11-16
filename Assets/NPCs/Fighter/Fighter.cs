5ddab88b059e3d926e1d0d8156cc3e61522b32f1
talford2 <talford2@hotmail.com>
2015-11-17 02:40:55 +1100

    Adding rage properties

--- Assets/NPCs/Fighter/Fighter.cs
+++ Assets/NPCs/Fighter/Fighter.cs
@@ -2,86 +2,99 @@
 
 public class Fighter : Npc<Fighter>
 {
-    public Vehicle VehiclePrefab;
+	public Vehicle VehiclePrefab;
 
-    private Vehicle _vehicleInstance;
-    public Vector3 Destination;
+	private Vehicle _vehicleInstance;
+	public Vector3 Destination;
 
-    public Transform Target;
+	public Transform Target;
 
-    public Vehicle VehicleInstance { get { return _vehicleInstance; } }
 
-    private void Awake()
-    {
-        _vehicleInstance = Utility.InstantiateInParent(VehiclePrefab.gameObject, transform).GetComponent<Vehicle>();
-        _vehicleInstance.GetComponent<Killable>().OnDie += OnVehicleDestroyed;
-        State = new FighterChase(this);
-    }
+	public float SightRange = 50f;
 
-    private void Update()
-    {
-        UpdateState();
-    }
+	public float AttackRange = 10f;
 
-    public Vector2 GetPitchYawToPoint(Vector3 point)
-    {
-        var toPoint = point - VehicleInstance.transform.position;
-        var yawDiff = Vector3.Dot(toPoint, VehicleInstance.transform.right);
-        var pitchDiff = Vector3.Dot(toPoint, VehicleInstance.transform.up);
 
-        var yawTolerance = 2f;
-        var pitchTolerance = 2f;
+	public Vehicle VehicleInstance { get { return _vehicleInstance; } }
 
-        var yawAmount = 0f;
-        var pitchAmount = 0f;
+	private void Awake()
+	{
+		_vehicleInstance = Utility.InstantiateInParent(VehiclePrefab.gameObject, transform).GetComponent<Vehicle>();
+		_vehicleInstance.GetComponent<Killable>().OnDie += OnVehicleDestroyed;
+		State = new FighterChase(this);
+	}
 
-        if (yawDiff < -yawTolerance)
-        {
-            yawAmount = Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
-        }
-        else if (yawDiff > yawTolerance)
-        {
-            yawAmount = -Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
-        }
+	private void Update()
+	{
+		UpdateState();
+	}
 
-        if (pitchDiff < -pitchTolerance)
-        {
-            pitchAmount = Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
-        }
-        else if (pitchDiff > pitchTolerance)
-        {
-            pitchAmount = -Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
-        }
+	public Vector2 GetPitchYawToPoint(Vector3 point)
+	{
+		var toPoint = point - VehicleInstance.transform.position;
+		var yawDiff = Vector3.Dot(toPoint, VehicleInstance.transform.right);
+		var pitchDiff = Vector3.Dot(toPoint, VehicleInstance.transform.up);
 
-        return new Vector2(pitchAmount, -yawAmount);
-    }
+		var yawTolerance = 2f;
+		var pitchTolerance = 2f;
 
-    private void OnVehicleDestroyed(Killable sender)
-    {
-        Destroy(gameObject);
-    }
+		var yawAmount = 0f;
+		var pitchAmount = 0f;
 
-    private void OnGUI()
-    {
-        GUI.Label(new Rect(Screen.width - 150f, 50f, 100f, 30f), State.Name);
-        GUI.Label(new Rect(Screen.width - 150f, 80f, 100f, 30f), string.Format("{0:f2}", VehicleInstance.GetVelocity().magnitude));
-    }
+		if (yawDiff < -yawTolerance)
+		{
+			yawAmount = Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
+		}
+		else if (yawDiff > yawTolerance)
+		{
+			yawAmount = -Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
+		}
 
-    private void OnDrawGizmos()
-    {
-        if (VehicleInstance != null)
-        {
-            switch (State.Name)
-            {
-                case "Evade":
-                    Gizmos.color = Color.magenta;
-                    break;
-                case "Chase":
-                    Gizmos.color = Color.red;
-                    break;
-            }
-            Gizmos.DrawLine(VehicleInstance.transform.position, Destination);
-            Gizmos.DrawSphere(Destination, 5f);
-        }
-    }
+		if (pitchDiff < -pitchTolerance)
+		{
+			pitchAmount = Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
+		}
+		else if (pitchDiff > pitchTolerance)
+		{
+			pitchAmount = -Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
+		}
+
+		return new Vector2(pitchAmount, -yawAmount);
+	}
+
+	private void OnVehicleDestroyed(Killable sender)
+	{
+		Destroy(gameObject);
+	}
+
+	private void OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width - 150f, 50f, 100f, 30f), State.Name);
+		GUI.Label(new Rect(Screen.width - 150f, 80f, 100f, 30f), string.Format("{0:f2}", VehicleInstance.GetVelocity().magnitude));
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (VehicleInstance != null)
+		{
+			switch (State.Name)
+			{
+				case "Evade":
+					Gizmos.color = Color.magenta;
+					break;
+				case "Chase":
+					Gizmos.color = Color.red;
+					break;
+			}
+			Gizmos.DrawLine(VehicleInstance.transform.position, Destination);
+			Gizmos.DrawSphere(Destination, 2f);
+
+
+			Gizmos.color = Color.blue;
+			Gizmos.DrawWireSphere(VehicleInstance.transform.position, SightRange);
+
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(VehicleInstance.transform.position, AttackRange);
+		}
+	}
 }
--- Assets/NPCs/Fighter/FighterChase.cs
+++ Assets/NPCs/Fighter/FighterChase.cs
@@ -2,48 +2,48 @@
 
 public class FighterChase : NpcState<Fighter>
 {
-    public FighterChase(Fighter npc) : base(npc)
-    {
-        Name = "Chase";
-    }
+	public FighterChase(Fighter npc) : base(npc)
+	{
+		Name = "Chase";
+	}
 
-    public override void Update()
-    {
-        var toTarget = Npc.Target.position - Npc.VehicleInstance.transform.position;
-        var dotTarget = Vector3.Dot(toTarget.normalized, Npc.VehicleInstance.transform.forward);
-        Debug.Log("DOT PROD: " + dotTarget);
+	public override void Update()
+	{
+		var toTarget = Npc.Target.position - Npc.VehicleInstance.transform.position;
+		var dotTarget = Vector3.Dot(toTarget.normalized, Npc.VehicleInstance.transform.forward);
+		Debug.Log("DOT PROD: " + dotTarget);
 
-        var targetDestination = Npc.Target.position;
-        Npc.Destination = Vector3.Lerp(Npc.Destination, targetDestination, Time.deltaTime);
+		var targetDestination = Npc.Target.position;
+		Npc.Destination = Vector3.Lerp(Npc.Destination, targetDestination, Time.deltaTime);
 
-        var pitchYaw = Npc.GetPitchYawToPoint(Npc.Destination);
-        Npc.VehicleInstance.YawThrottle = pitchYaw.y*Time.deltaTime;
-        Npc.VehicleInstance.PitchThotttle = pitchYaw.x*Time.deltaTime;
+		var pitchYaw = Npc.GetPitchYawToPoint(Npc.Destination);
+		Npc.VehicleInstance.YawThrottle = pitchYaw.y * Time.deltaTime;
+		Npc.VehicleInstance.PitchThotttle = pitchYaw.x * Time.deltaTime;
 
-        if (dotTarget < 0f)
-        {
-            if (toTarget.sqrMagnitude > 50f*50f)
-            {
-                Npc.VehicleInstance.TriggerAccelerate = true;
-            }
-            else
-            {
-                Npc.VehicleInstance.TriggerAccelerate = false;
-            }
+		if (dotTarget < 0f)
+		{
+			if (toTarget.sqrMagnitude > Npc.SightRange * Npc.SightRange)
+			{
+				Npc.VehicleInstance.TriggerAccelerate = true;
+			}
+			else
+			{
+				Npc.VehicleInstance.TriggerAccelerate = false;
+			}
 
-            if (toTarget.sqrMagnitude < 10f*10f)
-            {
-                Npc.VehicleInstance.TriggerBrake = true;
-            }
-            else
-            {
-                Npc.VehicleInstance.TriggerBrake = false;
-            }
-        }
-        else
-        {
-            //Npc.State = new FighterEvade(Npc);
-        }
-    }
-    
+			if (toTarget.sqrMagnitude < Npc.AttackRange * Npc.AttackRange)
+			{
+				Npc.VehicleInstance.TriggerBrake = true;
+			}
+			else
+			{
+				Npc.VehicleInstance.TriggerBrake = false;
+			}
+		}
+		else
+		{
+			//Npc.State = new FighterEvade(Npc);
+		}
+	}
+
 }
