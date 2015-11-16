d8d9e91e056b72457b1b447d5cfb2d48c8c03451
talford2 <talford2@hotmail.com>
2015-11-17 00:16:54 +1100

    Fixing line render for distant scale (crappy solution)

--- Assets/Scripts/DistantScaling.cs
+++ Assets/Scripts/DistantScaling.cs
@@ -1,59 +1,78 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DistantScaling : MonoBehaviour
 {
-    private Shiftable _shiftable;
-    private float _thresholdDistance;
-    private float _thresholdDistanceSquared;
-    private float _distantScale;
+	private Shiftable _shiftable;
+	private float _thresholdDistance;
+	private float _thresholdDistanceSquared;
+	private float _distantScale;
 
-    private int _distantLayer;
-    private int _defaultLayer;
+	private int _distantLayer;
+	private int _defaultLayer;
 
-    private void Awake()
-    {
-        _shiftable = GetComponent<Shiftable>();
-        _thresholdDistance = 1000f;
-        _distantScale = 1000f;
+	// Crappy solution
+	public float LineRendererWidth = 35f;
+	public List<LineRenderer> LineRenderers;
 
-        _thresholdDistanceSquared = _thresholdDistance*_thresholdDistance;
-        _distantLayer = LayerMask.NameToLayer("Distant");
-        _defaultLayer = LayerMask.NameToLayer("Default");
-    }
+	private void Awake()
+	{
+		_shiftable = GetComponent<Shiftable>();
+		_thresholdDistance = 1000f;
+		_distantScale = 1000f;
 
-    private void Start()
-    {
-        Universe.Current.ViewPort.OnMove += UpdatePositionAndScale;
-    }
+		_thresholdDistanceSquared = _thresholdDistance * _thresholdDistance;
+		_distantLayer = LayerMask.NameToLayer("Distant");
+		_defaultLayer = LayerMask.NameToLayer("Default");
+	}
 
-    private void UpdatePositionAndScale()
-    {
-        // Scaling
-        var worldDestination = Universe.Current.GetWorldPosition(_shiftable.UniverseCellIndex, _shiftable.CellLocalPosition);
-        var toCamera = worldDestination - Universe.Current.ViewPort.transform.position;
+	private void Start()
+	{
+		Universe.Current.ViewPort.OnMove += UpdatePositionAndScale;
+	}
 
-        if (toCamera.sqrMagnitude > _thresholdDistanceSquared)
-        {
-            var distance = toCamera.magnitude;
+	private void UpdatePositionAndScale()
+	{
+		// Scaling
+		var worldDestination = Universe.Current.GetWorldPosition(_shiftable.UniverseCellIndex, _shiftable.CellLocalPosition);
+		var toCamera = worldDestination - Universe.Current.ViewPort.transform.position;
 
-            // This will only be able to scale objects of a radius up to the _thresholdDistance.
-            // Keep the scaled object 1 unit in front of the camera!
-            var scaledDistance = Mathf.Abs(distance - _thresholdDistance - 1f)/_distantScale + 1f; // + _thresholdDistance;
+		if (toCamera.sqrMagnitude > _thresholdDistanceSquared)
+		{
+			var distance = toCamera.magnitude;
 
-            var scale = Mathf.Clamp(scaledDistance/distance, 0f, 1f);
-            transform.localScale = new Vector3(scale, scale, scale);
+			// This will only be able to scale objects of a radius up to the _thresholdDistance.
+			// Keep the scaled object 1 unit in front of the camera!
+			var scaledDistance = Mathf.Abs(distance - _thresholdDistance - 1f) / _distantScale + 1f; // + _thresholdDistance;
 
-            // Positioning
-            transform.position = Universe.Current.ViewPort.transform.position + toCamera.normalized * scaledDistance;
-            if (gameObject.layer != _distantLayer)
-                Utility.SetLayerRecursively(gameObject, _distantLayer);
-        }
-        else
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-            transform.position = worldDestination;
-            if (gameObject.layer != _defaultLayer)
-                Utility.SetLayerRecursively(gameObject, _defaultLayer);
-        }
-    }
+			var scale = Mathf.Clamp(scaledDistance / distance, 0f, 1f);
+			transform.localScale = new Vector3(scale, scale, scale);
+
+			// Crappy solution
+			var lineRenderWidth = scale * LineRendererWidth;
+			foreach (var lineRenderer in LineRenderers)
+			{
+				lineRenderer.SetWidth(lineRenderWidth, lineRenderWidth);
+			}
+
+			// Positioning
+			transform.position = Universe.Current.ViewPort.transform.position + toCamera.normalized * scaledDistance;
+			if (gameObject.layer != _distantLayer)
+				Utility.SetLayerRecursively(gameObject, _distantLayer);
+		}
+		else
+		{
+			transform.localScale = new Vector3(1f, 1f, 1f);
+
+			// Crappy solution
+			foreach (var lineRenderer in LineRenderers)
+			{
+				lineRenderer.SetWidth(LineRendererWidth, LineRendererWidth);
+			}
+
+			transform.position = worldDestination;
+			if (gameObject.layer != _defaultLayer)
+				Utility.SetLayerRecursively(gameObject, _defaultLayer);
+		}
+	}
 }
