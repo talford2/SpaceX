a8ed3800a119e3755e7680dca455b0d26415d230
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-16 21:29:09 +0800

    Very minor optimization.

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -50,10 +50,11 @@ public class PlayerController : MonoBehaviour
     {
         var mouseRay = Universe.Current.ViewPort.AttachedCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         RaycastHit aimHit;
-        var aimAtPosition = _playVehicleInstance.CurrentWeapon.GetShootPointCentre() + _playVehicleInstance.transform.forward * MaxAimDistance;
+        var weaponShootPointsCentre = _playVehicleInstance.CurrentWeapon.GetShootPointCentre();
+        var aimAtPosition = weaponShootPointsCentre + _playVehicleInstance.transform.forward*MaxAimDistance;
         if (Physics.Raycast(mouseRay, out aimHit, MaxAimDistance, ~LayerMask.GetMask("Player")))
         {
-            var toAim = aimHit.point - _playVehicleInstance.CurrentWeapon.GetShootPointCentre();
+            var toAim = aimHit.point - weaponShootPointsCentre;
             var dotProd = Vector3.Dot(toAim, _playVehicleInstance.transform.forward);
             if (dotProd > MinAimDistance)
             {
