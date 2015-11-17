e45caef4a1d9fbbd43f2c00264c9c9520e6bd560
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-18 00:47:38 +0800

    Add tracker arrows.

--- Assets/HUD/UniverseTrackers.cs
+++ Assets/HUD/UniverseTrackers.cs
@@ -3,15 +3,15 @@ using UnityEngine.UI;
 
 public class UniverseTrackers : MonoBehaviour
 {
-	public Transform Target;
+    public Transform Target;
 
-	private Camera _cam;
+    private Camera _cam;
 
     private Vector3 screenCentre;
     private float boundaryPadding = 20f;
     private Rect screenBounds;
 
-	public Image Cursor;
+    public Image Cursor;
 
     private void Start()
     {
@@ -23,12 +23,22 @@ public class UniverseTrackers : MonoBehaviour
         ccc.OnMove += Ccc_OnMove;
     }
 
-	private void Ccc_OnMove()
-	{
-		var r = _cam.WorldToScreenPoint(Target.position);
+    private void Ccc_OnMove()
+    {
+        var r = _cam.WorldToScreenPoint(Target.position);
         Cursor.rectTransform.localPosition = ClampToScreen(r);
-	    Cursor.rectTransform.localRotation = Quaternion.AngleAxis(GetScreenAngle(r), Vector3.forward);
-	}
+
+        if (r.z < 0f)
+            r *= -1f;
+        if (!screenBounds.Contains(r))
+        {
+            Cursor.rectTransform.localRotation = Quaternion.AngleAxis(GetScreenAngle(r - Cursor.rectTransform.localPosition), Vector3.forward);
+        }
+        else
+        {
+            Cursor.rectTransform.localRotation = Quaternion.identity;
+        }
+    }
 
     private Vector2 ClampToScreen(Vector3 pointAtPos)
     {
@@ -36,7 +46,7 @@ public class UniverseTrackers : MonoBehaviour
             pointAtPos *= -1f;
 
         var delta = pointAtPos - screenCentre;
-        var gradient = delta.y / delta.x;
+        var gradient = delta.y/delta.x;
 
         if (!screenBounds.Contains(pointAtPos))
         {
@@ -64,7 +74,7 @@ public class UniverseTrackers : MonoBehaviour
             }
 
             result.x = Mathf.Clamp(result.x, screenBounds.xMin - screenCentre.x, screenBounds.xMax - screenCentre.x);
-            result.y = Mathf.Clamp(result.y, screenBounds.yMin- screenCentre.y, screenBounds.yMax - screenCentre.y);
+            result.y = Mathf.Clamp(result.y, screenBounds.yMin - screenCentre.y, screenBounds.yMax - screenCentre.y);
             return result;
         }
         return pointAtPos - screenCentre;
@@ -76,7 +86,7 @@ public class UniverseTrackers : MonoBehaviour
             pointAtPos *= -1f;
 
         var delta = pointAtPos - screenCentre;
-        var angle = Mathf.Rad2Deg*Mathf.Atan2(delta.x, -delta.y);
+        var angle = Mathf.Rad2Deg*Mathf.Atan2(delta.x, -delta.y) + 180f;
         return angle;
     }
 }
