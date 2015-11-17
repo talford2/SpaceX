4262aa6d1ebf00ba60f9eaba80bd5c44f3720b0d
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-17 22:42:38 +0800

    Failed attempt at clamping tracker to screen.

--- Assets/HUD/UniverseTrackers.cs
+++ Assets/HUD/UniverseTrackers.cs
@@ -7,32 +7,81 @@ public class UniverseTrackers : MonoBehaviour
 
 	private Camera _cam;
 
+    private Vector3 screenCentre;
+    private Rect screenBounds;
+
 	public UnityEngine.UI.Image Cursor;
 
 	// Use this for initialization
 	void Start()
 	{
+        screenCentre = new Vector3(Screen.width/2f, Screen.height/2f, 0f);
+	    var boundaryPadding = 10f;
+        screenBounds = new Rect(0 + boundaryPadding, 0 + boundaryPadding, Screen.width - 2f * boundaryPadding, Screen.height - 2f * +boundaryPadding);
+
 		var ccc = Universe.Current.ViewPort;
 		_cam = ccc.GetComponent<Camera>();
 		ccc.OnMove += Ccc_OnMove;
 	}
 
+    private Vector2 clampPos;
+
 	private void Ccc_OnMove()
 	{
-		//throw new System.NotImplementedException();
 		var r = _cam.WorldToScreenPoint(Target.position);
 
-		var halfWidth = Screen.width / 2f;
-		var halfHeight = Screen.height / 2f;
+		//var v = new Vector3(r.x - screenCentre.x, r.y - screenCentre.y, 0);
+		//var v = new Vector3(Mathf.Clamp(r.x - halfWidth, Screen.width / -2f, Screen.width / 2f), Mathf.Clamp(r.y - halfHeight, Screen.height / -2f, Screen.height / 2f), 0);
 
-		//var v = new Vector3(r.x - halfWidth, r.y - halfHeight, 0);
-		var v = new Vector3(Mathf.Clamp(r.x - halfWidth, Screen.width / -2f, Screen.width / 2f), Mathf.Clamp(r.y - halfHeight, Screen.height / -2f, Screen.height / 2f), 0);
+	    var v = ClampToScreen(r);
+	    clampPos = v;
 
 		Cursor.rectTransform.localPosition = v;
 	}
 
-	// Update is called once per frame
-	void LateUpdate()
-	{
-	}
+    private Vector2 ClampToScreen(Vector3 pointAtPos)
+    {
+        if (pointAtPos.z < 0f)
+            pointAtPos *= -1f;
+
+        if (!screenBounds.Contains(pointAtPos))
+        {
+            var delta = pointAtPos;
+            var angle = Mathf.Atan2(delta.x, -delta.y);
+
+            var cos = Mathf.Cos(angle);
+            var sin = Mathf.Sin(angle);
+
+            var gradient = sin/cos;
+
+            if (cos > 0f)
+            {
+                pointAtPos = new Vector2(screenCentre.y/gradient, screenCentre.y);
+            }
+            else
+            {
+                pointAtPos = new Vector2(-screenCentre.y/gradient, -screenCentre.y);
+            }
+
+            if (pointAtPos.x > screenCentre.x)
+            {
+                pointAtPos = new Vector2(screenCentre.x, screenCentre.x*gradient);
+            }
+            else if (pointAtPos.x < -screenCentre.x)
+            {
+                pointAtPos = new Vector2(-screenCentre.x, -screenCentre.x*gradient);
+            }
+
+            return new Vector2(
+                Mathf.Clamp(pointAtPos.x , screenBounds.x - screenCentre.x, screenBounds.width - screenCentre.x),
+                Mathf.Clamp(Screen.height - pointAtPos.y, screenBounds.y - screenCentre.y, screenBounds.height - screenCentre.y)
+                );
+        }
+        return pointAtPos - screenCentre;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(20f, 50f, 100f, 25f), clampPos.ToString());
+    }
 }
