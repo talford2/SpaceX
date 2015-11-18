c828d6154fae111f8baa717f5b75a42ca59ea29f
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-18 22:38:27 +0800

    Minor tidy up.

--- Assets/NPCs/Fighter/Fighter.cs
+++ Assets/NPCs/Fighter/Fighter.cs
@@ -46,35 +46,6 @@ public class Fighter : Npc<Fighter>
 
 	public Vector2 GetPitchYawToPoint(Vector3 point)
 	{
-        /*
-		var toPoint = point - VehicleInstance.transform.position;
-		var yawDiff = Vector3.Dot(toPoint, VehicleInstance.transform.right);
-		var pitchDiff = Vector3.Dot(toPoint, VehicleInstance.transform.up);
-
-		var yawTolerance = 2f;
-		var pitchTolerance = 2f;
-
-		var yawAmount = 0f;
-		var pitchAmount = 0f;
-
-		if (yawDiff < -yawTolerance)
-		{
-			yawAmount = Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
-		}
-		else if (yawDiff > yawTolerance)
-		{
-            yawAmount = -Mathf.Clamp(Mathf.Abs(yawDiff) / yawTolerance, 0f, 1f);
-		}
-
-		if (pitchDiff < -pitchTolerance)
-		{
-            pitchAmount = Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
-		}
-		else if (pitchDiff > pitchTolerance)
-		{
-            pitchAmount = -Mathf.Clamp(Mathf.Abs(pitchDiff) / pitchTolerance, 0f, 1f);
-		}
-        */
         var toPoint = point - VehicleInstance.transform.position;
 	    var yawAmount = Vector3.Dot(toPoint.normalized, VehicleInstance.transform.right);
         var pitchAmount = Vector3.Dot(-toPoint.normalized, VehicleInstance.transform.up);
