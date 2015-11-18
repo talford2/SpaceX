015ff5fd25b39ea26553b86b7a005cad25fa8172
talford2 <talford2@hotmail.com>
2015-11-19 04:00:44 +1100

    Removing tracker from player

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -11,118 +11,121 @@ public class PlayerController : MonoBehaviour
 	private int _curVehicleIndex = 0;
 
 	public bool InvertY = false;
-    public bool HideMouse = false;
+	public bool HideMouse = false;
 
-    [Header("Aiming")]
-    public float AimSensitivity = 10f;
-    public float MinAimDistance = 10f;
-    public float MaxAimDistance = 1000f;
+	[Header("Aiming")]
+	public float AimSensitivity = 10f;
+	public float MinAimDistance = 10f;
+	public float MaxAimDistance = 1000f;
 
-    private float screenAspect;
+	private float screenAspect;
 
-    private void Awake()
-    {
-        _playVehicleInstance = Instantiate<Vehicle>(VehiclePrefab);
-        _playVehicleInstance.gameObject.layer = LayerMask.NameToLayer("Player");
-        _current = this;
-        Cursor.visible = !HideMouse;
-        if (HideMouse)
-            Cursor.lockState = CursorLockMode.Locked;
-
-        PlayerVehicles.Insert(0, _playVehicleInstance);
-
-        screenAspect = (float)Screen.height/(float)Screen.width;
-        //Debug.Break();
-    }
-
-    private void Start()
+	private void Awake()
 	{
-        //Universe.Current.ViewPort.Tar
+		_playVehicleInstance = Instantiate<Vehicle>(VehiclePrefab);
+		Destroy(_playVehicleInstance.GetComponent<Tracker>());
+
+		_playVehicleInstance.gameObject.layer = LayerMask.NameToLayer("Player");
+		_current = this;
+		Cursor.visible = !HideMouse;
+		if (HideMouse)
+			Cursor.lockState = CursorLockMode.Locked;
+
+		PlayerVehicles.Insert(0, _playVehicleInstance);
+
+		screenAspect = (float)Screen.height / (float)Screen.width;
+
+		//Debug.Break();
+	}
+
+	private void Start()
+	{
+		//Universe.Current.ViewPort.Tar
 		//FollowCamera.Current.Target = _playVehicleInstance.transform;
 	}
 
-    private Vector3 GetAimAt()
-    {
-        var mouseRay = Universe.Current.ViewPort.AttachedCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit aimHit;
-        var weaponShootPointsCentre = _playVehicleInstance.CurrentWeapon.GetShootPointCentre();
-        var aimAtPosition = weaponShootPointsCentre + _playVehicleInstance.transform.forward*MaxAimDistance;
-        if (Physics.Raycast(mouseRay, out aimHit, MaxAimDistance, ~LayerMask.GetMask("Player")))
-        {
-            var toAim = aimHit.point - weaponShootPointsCentre;
-            var dotProd = Vector3.Dot(toAim, _playVehicleInstance.transform.forward);
-            if (dotProd > MinAimDistance)
-            {
-                aimAtPosition = aimHit.point;
-            }
-        }
-        return aimAtPosition;
-    }
+	private Vector3 GetAimAt()
+	{
+		var mouseRay = Universe.Current.ViewPort.AttachedCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+		RaycastHit aimHit;
+		var weaponShootPointsCentre = _playVehicleInstance.CurrentWeapon.GetShootPointCentre();
+		var aimAtPosition = weaponShootPointsCentre + _playVehicleInstance.transform.forward * MaxAimDistance;
+		if (Physics.Raycast(mouseRay, out aimHit, MaxAimDistance, ~LayerMask.GetMask("Player")))
+		{
+			var toAim = aimHit.point - weaponShootPointsCentre;
+			var dotProd = Vector3.Dot(toAim, _playVehicleInstance.transform.forward);
+			if (dotProd > MinAimDistance)
+			{
+				aimAtPosition = aimHit.point;
+			}
+		}
+		return aimAtPosition;
+	}
 
-    private void Update()
-    {
+	private void Update()
+	{
 		if (Input.GetKeyDown(KeyCode.LeftControl))
 		{
 			Debug.Break();
 		}
 
-        var mouseClamp = 0.003f;
-        var mouseHorizontal = AimSensitivity*Mathf.Clamp(Input.GetAxis("MouseHorizontal")/Screen.width, -mouseClamp, mouseClamp);
-        var mouseVertical = AimSensitivity*Mathf.Clamp(screenAspect*Input.GetAxis("MouseVertical")/Screen.height, -mouseClamp, mouseClamp);
+		var mouseClamp = 0.003f;
+		var mouseHorizontal = AimSensitivity * Mathf.Clamp(Input.GetAxis("MouseHorizontal") / Screen.width, -mouseClamp, mouseClamp);
+		var mouseVertical = AimSensitivity * Mathf.Clamp(screenAspect * Input.GetAxis("MouseVertical") / Screen.height, -mouseClamp, mouseClamp);
 
-        if (InvertY)
-        {
-            _playVehicleInstance.PitchThotttle = (Input.GetAxis("Vertical") + mouseVertical)*-1;
-        }
-        else
-        {
-            _playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical") + mouseVertical;
-        }
-        _playVehicleInstance.YawThrottle = Input.GetAxis("Horizontal") + mouseHorizontal;
-        _playVehicleInstance.RollThrottle = Input.GetAxis("Roll") + Input.GetAxis("KeyboardRoll");
-        _playVehicleInstance.CurrentWeapon.IsTriggered = (Input.GetAxis("FireTrigger") + Input.GetAxis("MouseFireTrigger")) > 0;
+		if (InvertY)
+		{
+			_playVehicleInstance.PitchThotttle = (Input.GetAxis("Vertical") + mouseVertical) * -1;
+		}
+		else
+		{
+			_playVehicleInstance.PitchThotttle = Input.GetAxis("Vertical") + mouseVertical;
+		}
+		_playVehicleInstance.YawThrottle = Input.GetAxis("Horizontal") + mouseHorizontal;
+		_playVehicleInstance.RollThrottle = Input.GetAxis("Roll") + Input.GetAxis("KeyboardRoll");
+		_playVehicleInstance.CurrentWeapon.IsTriggered = (Input.GetAxis("FireTrigger") + Input.GetAxis("MouseFireTrigger")) > 0;
 
-        _playVehicleInstance.SetAimAt(GetAimAt());
+		_playVehicleInstance.SetAimAt(GetAimAt());
 
-        _playVehicleInstance.TriggerAccelerate = false;
-        if (Input.GetButton("Accelerate") || Input.GetButton("KeyboardAccelerate"))
-        {
-            _playVehicleInstance.TriggerAccelerate = true;
-        }
+		_playVehicleInstance.TriggerAccelerate = false;
+		if (Input.GetButton("Accelerate") || Input.GetButton("KeyboardAccelerate"))
+		{
+			_playVehicleInstance.TriggerAccelerate = true;
+		}
 
-        _playVehicleInstance.TriggerBrake = false;
-        if (Input.GetButton("Brake") || Input.GetButton("KeyboardBrake"))
-        {
-            _playVehicleInstance.TriggerBrake = true;
-        }
+		_playVehicleInstance.TriggerBrake = false;
+		if (Input.GetButton("Brake") || Input.GetButton("KeyboardBrake"))
+		{
+			_playVehicleInstance.TriggerBrake = true;
+		}
 
-        _playVehicleInstance.TriggerBoost = false;
-        if (Input.GetButton("KeyboardBoost"))
-        {
-            _playVehicleInstance.TriggerBoost = true;
-        }
+		_playVehicleInstance.TriggerBoost = false;
+		if (Input.GetButton("KeyboardBoost"))
+		{
+			_playVehicleInstance.TriggerBoost = true;
+		}
 
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            _curVehicleIndex++;
-            if (_curVehicleIndex >= PlayerVehicles.Count)
-            {
-                _curVehicleIndex = 0;
-            }
-            _playVehicleInstance = PlayerVehicles[_curVehicleIndex];
-            //FollowCamera.Current.Target = _playVehicleInstance.transform;
-        }
+		if (Input.GetKeyUp(KeyCode.E))
+		{
+			_curVehicleIndex++;
+			if (_curVehicleIndex >= PlayerVehicles.Count)
+			{
+				_curVehicleIndex = 0;
+			}
+			_playVehicleInstance = PlayerVehicles[_curVehicleIndex];
+			//FollowCamera.Current.Target = _playVehicleInstance.transform;
+		}
 
-        //var vehicle
-        // Check for shifting
-    }
+		//var vehicle
+		// Check for shifting
+	}
 
-    public bool InPlayerActiveCells(CellIndex checkCell)
+	public bool InPlayerActiveCells(CellIndex checkCell)
 	{
 		var playerCellIndex = _playVehicleInstance.Shiftable.UniverseCellIndex;
 		for (var x = -1; x < 2; x++)
@@ -151,7 +154,7 @@ public class PlayerController : MonoBehaviour
 		get { return _playVehicleInstance; }
 	}
 
-    /*
+	/*
     private void OnGUI()
     {
         GUI.Label(new Rect(Screen.width - 100f, Screen.height - 100f, 100f, 25), string.Format("{0:f2} m/s", VehicleInstance.GetVelocity().magnitude));
