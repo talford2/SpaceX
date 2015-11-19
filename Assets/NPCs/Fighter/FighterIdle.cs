91b10b59975602b40e1531c015c95a48666b6183
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-20 00:40:20 +0800

    Set target to player from idle, if player exists.

--- Assets/NPCs/Fighter/FighterIdle.cs
+++ Assets/NPCs/Fighter/FighterIdle.cs
@@ -10,5 +10,10 @@ public class FighterIdle : NpcState<Fighter>
     public override void Update()
     {
         Npc.VehicleInstance.PitchThotttle = 1f*Time.deltaTime;
+        if (PlayerController.Current.VehicleInstance != null)
+        {
+            Npc.Target = PlayerController.Current.VehicleInstance.transform;
+            Npc.State = new FighterChase(Npc);
+        }
     }
 }
