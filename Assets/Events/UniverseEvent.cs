a6b0e9a8e60ae72a5b893620a6078c2fc853edb3
talford2 <talford2@hotmail.com>
2015-11-20 02:47:02 +1100

    Some fixes

---
+++ Assets/Events/UniverseEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UniverseEvent : MonoBehaviour
+{
+	private void OnDrawGizmos()
+	{
+		var children = GetComponentsInChildren<Transform>();
+		Gizmos.color = Color.red;
+		foreach (var child in children)
+		{
+			Gizmos.DrawLine(transform.position, child.position);
+		}
+	}
+}
