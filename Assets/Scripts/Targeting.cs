99a3c2d509b298c7b161e78c3a9a375a8a6b88c9
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-22 02:01:55 +0800

    Creating targeting components.

---
+++ Assets/Scripts/Targeting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeting
+{
+    private static Dictionary<Team, List<Transform>> targetables;
+
+    public static void AddTargetable(Team team, Transform transform)
+    {
+        if (targetables == null)
+            targetables = new Dictionary<Team, List<Transform>>();
+        if (!targetables.ContainsKey(team))
+            targetables.Add(team, new List<Transform>());
+        targetables[team].Add(transform);
+    }
+
+    public static void RemoveTargetable(Team team, Transform transform)
+    {
+        if (targetables.ContainsKey(team))
+            targetables[team].Remove(transform);
+    }
+}
