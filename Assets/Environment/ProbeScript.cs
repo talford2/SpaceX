69f83b8d7ccf6a8485dda91d37ff65db58e2f607
talford2 <talford2@hotmail.com>
2015-11-29 18:59:33 +1100

    Some minor visual tweaks

---
+++ Assets/Environment/ProbeScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProbeScript : MonoBehaviour
+{
+	private ReflectionProbe _probe;
+
+	public RenderTexture _texture;
+
+	public GameObject Background;
+
+
+	public Material RenderedMaterialBackground;
+
+	private int _renderId;
+
+	private bool _hasRendered = false;
+
+	void Awake()
+	{
+		_probe = GetComponent<ReflectionProbe>();
+
+	}
+	void Start()
+	{
+		_renderId = _probe.RenderProbe(_texture);
+
+		//_texture = _probe.texture;
+		//Background.SetActive(false);
+		//Destroy(Background);
+
+
+	}
+
+	void Update()
+	{
+		if (!_hasRendered && _probe.IsFinishedRendering(_renderId))
+		{
+			Background.SetActive(false);
+
+
+			RenderedMaterialBackground.SetColor("_TintColor", Color.red);
+			RenderedMaterialBackground.SetColor("_Color", Color.red);
+			RenderedMaterialBackground.SetTexture("_Cube", _probe.texture);
+
+
+			Debug.Log("Rendered background");
+			_hasRendered = transform;
+		}
+	}
+}
