aa2a7ab3626ccdf033c85b82620d9f020b4310ee
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-30 16:12:28 +0800

    Add list of star prefabs to StarField.

--- Assets/Environment/Stars/Starfield.cs
+++ Assets/Environment/Stars/Starfield.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Starfield : MonoBehaviour
 {
-    public GameObject StarPrefab;
+    public List<GameObject> StarPrefabs;
     public float MinSize;
     public float MaxSize;
     public float Radius;
@@ -13,7 +14,7 @@ public class Starfield : MonoBehaviour
         for (var i = 0; i < Count; i++)
         {
             var position = Radius*Random.onUnitSphere;
-            var star = Utility.InstantiateInParent(StarPrefab, position, Quaternion.identity, transform, LayerMask.NameToLayer("Universe Background"));
+            var star = Utility.InstantiateInParent(StarPrefabs[Random.Range(0, StarPrefabs.Count)], position, Quaternion.identity, transform, LayerMask.NameToLayer("Universe Background"));
             star.transform.localScale = Vector3.one*Random.Range(MinSize, MaxSize);
             star.transform.LookAt(Camera.main.transform, transform.up);
         }
