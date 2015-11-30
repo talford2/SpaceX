667b6440632f3fa639286a99fbe11b84a37677f1
Steven Alford <steven.alford@collinsonlatitude.com>
2015-11-30 14:41:57 +0800

    Create Starfield.

---
+++ Assets/Environment/Stars/Starfield.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Starfield : MonoBehaviour
+{
+    public GameObject StarPrefab;
+    public float MinSize;
+    public float MaxSize;
+    public float Radius;
+    public int Count;
+
+    private void Awake()
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            var position = Radius*Random.onUnitSphere;
+            var star = Utility.InstantiateInParent(StarPrefab, position, Quaternion.identity, transform, LayerMask.NameToLayer("Universe Background"));
+            star.transform.localScale = Vector3.one*Random.Range(MinSize, MaxSize);
+            star.transform.LookAt(Camera.main.transform, transform.up);
+        }
+    }
+}
