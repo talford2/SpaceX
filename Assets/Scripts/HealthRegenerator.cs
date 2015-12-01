9a6e71ca171230e5dd45ac34b20903df5b6e1d17
Steven Alford <steven.alford@collinsonlatitude.com>
2015-12-02 01:48:49 +0800

    Create HealthRegenerator.

---
+++ Assets/Scripts/HealthRegenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Killable))]
+public class HealthRegenerator : MonoBehaviour
+{
+    public float RegenerationRate;
+    public float RegenerationDelay;
+
+    private Killable killable;
+    private float delayCooldown;
+
+    private void Awake()
+    {
+        killable = GetComponent<Killable>();
+        killable.OnDamage += HealthRegenerator_OnDamage;
+    }
+
+    private void Update()
+    {
+        if (delayCooldown >= 0f)
+        {
+            delayCooldown -= Time.deltaTime;
+        }
+        if (delayCooldown < 0f)
+        {
+            if (killable.Health < killable.MaxHealth)
+            {
+                killable.Health += RegenerationRate*Time.deltaTime;
+                if (killable.Health > killable.MaxHealth)
+                    killable.Health = killable.MaxHealth;
+            }
+        }
+    }
+
+    private void HealthRegenerator_OnDamage(Vector3 position, Vector3 normal)
+    {
+        delayCooldown = RegenerationDelay;
+    }
+}
