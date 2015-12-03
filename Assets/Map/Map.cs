981397db5d008e1ff8792627d2ad6573b23f73d4
Steven Alford <steven.alford@collinsonlatitude.com>
2015-12-03 16:53:47 +0800

    More map stuff.

--- Assets/Map/Map.cs
+++ Assets/Map/Map.cs
@@ -4,9 +4,12 @@ using UnityEngine.UI;
 public class Map : MonoBehaviour
 {
     public Text MapSystemText;
+    public GameObject PlayerPinPrefab;
+    public GameObject SquadronPinPrefab;
     public GameObject PinPrefab;
 
     private Camera _mapCamera;
+    private MapCamera _mapCameraController;
     private static Map _current;
 
     public static Map Current
@@ -17,19 +20,39 @@ public class Map : MonoBehaviour
     private void Awake()
     {
         _mapCamera = GetComponentInChildren<Camera>();
+        _mapCameraController = _mapCamera.GetComponent<MapCamera>();
         _current = this;
         Hide();
     }
 
     private void Populate()
     {
+        var playerPin = AddPin(PlayerPinPrefab, PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition());
+        _mapCameraController.SetLookAt(playerPin.position);
+
+        foreach (var squadronMember in PlayerController.Current.Squadron)
+        {
+            var squadronVehicle = squadronMember.VehicleInstance;
+            if (squadronVehicle != null && squadronVehicle != PlayerController.Current.VehicleInstance)
+            {
+                AddPin(SquadronPinPrefab, squadronVehicle.Shiftable.GetWorldPosition());
+            }
+        }
+
         var universeEvents = Universe.Current.UniverseEvents;
         foreach (var universeEvent in universeEvents)
         {
-            var pin = Utility.InstantiateInParent(PinPrefab, transform);
-            pin.layer = LayerMask.NameToLayer("Map");
-            pin.transform.position = 0.01f*universeEvent.Shiftable.GetWorldPosition();
+            AddPin(PinPrefab, universeEvent.Shiftable.GetWorldPosition());
         }
+    }
+
+    private Transform AddPin(GameObject prefab, Vector3 worldPosition)
+    {
+        var mapScale = 0.01f;
+        var pin = Utility.InstantiateInParent(prefab, transform);
+        pin.layer = LayerMask.NameToLayer("Map");
+        pin.transform.position = mapScale * worldPosition;
+        return pin.transform;
     }
 
     public void Show()
