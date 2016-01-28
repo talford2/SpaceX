35217a95b41638019fc344fdfae00a4436aa5c2c
Timothy <talford2@hotmail.com>
2016-01-29 00:26:59 +1100

    Setting project to Linear colour space.

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour
+{
+	#region Public Variables
+
+	public float PositionAmplitude = 0.3f;
+
+	public float RotateAmplitude = 1f;
+
+	public float frequency = 10f;
+
+	public float Duration = 2f;
+
+	public AnimationCurve ShakeAmplitude;
+
+	#endregion
+
+	#region Private Variables
+
+	private float _cooldown = 0;
+
+	#endregion
+
+	void Update()
+	{
+		if (_cooldown > 0)
+		{
+			var frac = 1f - _cooldown / Duration;
+
+			var amp = ShakeAmplitude.Evaluate(frac);
+
+			var freq = frequency * Mathf.Rad2Deg / 90f;
+
+			var x = Mathf.Sin(freq);
+			var y = Mathf.Sin(freq * 1.2f);
+			var z = Mathf.Sin(freq * 1.4f);
+
+			var fracVec = new Vector3(x, y, z) * amp;
+
+			transform.localPosition = fracVec * PositionAmplitude;
+			transform.localRotation = Quaternion.Euler(fracVec * RotateAmplitude);
+
+			_cooldown -= Time.deltaTime;
+			_cooldown = Mathf.Max(_cooldown, 0);
+		}
+	}
+
+	public void TriggerShake(float duration, float amplitude)
+	{
+		PositionAmplitude = amplitude;
+		_cooldown = duration;
+	}
+}
---
+++ Assets/Scripts/SpriteMaterial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteMaterial : MonoBehaviour
+{
+
+	public Material Material;
+
+	public int Collumns = 2;
+
+	public int Rows = 2;
+
+	public int Frames = 4;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
