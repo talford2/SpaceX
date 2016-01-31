6a0bc8ab22dd421274132783fb822faab8b848bb
Steven Alford <steven.alford@collinsonlatitude.com>
2016-01-31 22:23:47 +0800

    Add distance label to nav point.

--- Assets/HUD/NavPointTracker.cs
+++ Assets/HUD/NavPointTracker.cs
@@ -5,19 +5,24 @@ public class NavPointTracker : Tracker
 {
     public Texture2D TrackerCursorImage;
     public Texture2D ArrowCursorImage;
+    public Font LabelFont;
 
     private Vector2 screenCentre;
     private Rect screenBounds;
     private Image imageInstance;
+    private Text labelInstance;
 
     private Sprite trackerSprite;
     private Sprite arrowSprite;
+
+    private Shiftable shiftable;
 
     private void Awake()
     {
         screenCentre = new Vector3(0.5f * Screen.width, 0.5f * Screen.height);
         var boundaryPadding = 20f;
         screenBounds = new Rect(boundaryPadding, boundaryPadding, Screen.width - 2f * boundaryPadding, Screen.height - 2f * boundaryPadding);
+        shiftable = GetComponent<Shiftable>();
     }
 
     public override Image CreateInstance()
@@ -34,6 +39,19 @@ public class NavPointTracker : Tracker
         trackerImg.sprite = trackerSprite;
         trackerImg.SetNativeSize();
 
+        var labelObject = new GameObject(string.Format("{0}_Label", transform.name));
+        var labelText = labelObject.AddComponent<Text>();
+
+        labelText.font = LabelFont;
+        labelText.fontSize = 10;
+        labelText.rectTransform.pivot = new Vector2(0.5f, 0.9f);
+        labelText.color = new Color(1f, 1f, 1f, 1f);
+        labelText.alignment = TextAnchor.MiddleCenter;
+        labelText.rectTransform.localScale = Vector3.one;
+
+        labelObject.transform.SetParent(trackerObj.transform);
+
+        labelInstance = labelText;
         imageInstance = trackerImg;
         return trackerImg;
     }
@@ -50,12 +68,18 @@ public class NavPointTracker : Tracker
             imageInstance.sprite = trackerSprite;
             imageInstance.rectTransform.localPosition = screenPosition - new Vector3(screenCentre.x, screenCentre.y, 0f);
             imageInstance.rectTransform.localRotation = Quaternion.identity;
+
+            var fromPlayer = shiftable.GetAbsoluteUniversePosition() - PlayerController.Current.VehicleInstance.Shiftable.GetAbsoluteUniversePosition();
+            labelInstance.text = DistanceDisplay.GetDistanceString(fromPlayer.magnitude);
+            labelInstance.enabled = true;
         }
         else
         {
             imageInstance.sprite = arrowSprite;
             imageInstance.rectTransform.localPosition = Utility.GetBoundsIntersection(screenPosition, screenBounds);
             imageInstance.rectTransform.localRotation = Quaternion.Euler(0f, 0f, GetScreenAngle(screenPosition));
+
+            labelInstance.enabled = false;
         }
     }
 
---
+++ Assets/Map/DistanceDisplay.cs
@@ -0,0 +1,23 @@
+﻿public class DistanceDisplay
+{
+    public static string GetDistanceString(float distance)
+    {
+        if (distance < 10f)
+            return string.Format("{0:n2}m", distance);
+        if (distance < 100f)
+            return string.Format("{0:n1}m", distance);
+        if (distance < 1000f)
+            return string.Format("{0:n0}m", distance);
+        if (distance < 10000f)
+            return string.Format("{0:n2}km", distance/1000f);
+        if (distance < 100000f)
+            return string.Format("{0:n1}km", distance/1000f);
+        if (distance < 1000000f)
+            return string.Format("{0:n0}km", distance/1000f);
+        if (distance < 10000000f)
+            return string.Format("{0:n2}Mm", distance/1000000f);
+        if (distance < 100000000f)
+            return string.Format("{0:n1}Mm", distance/1000000f);
+        return string.Format("{0:n0}Mm", distance/1000000f);
+    }
+}
