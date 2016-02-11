27969f9ec2c62bd9a10a06d82b76db348d33cc5c
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-11 16:31:19 +0800

    Add event tracker.

---
+++ Assets/HUD/EventTracker.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventTracker : Tracker
+{
+    public Texture2D TrackerCursorImage;
+    public Texture2D ArrowCursorImage;
+    public Font LabelFont;
+
+    private Vector2 screenCentre;
+    private Rect screenBounds;
+    private Image imageInstance;
+    private Text labelInstance;
+
+    private Sprite trackerSprite;
+    private Sprite arrowSprite;
+
+    private Shiftable shiftable;
+
+    private bool isVisible;
+    private bool isFading;
+
+    private void Awake()
+    {
+        screenCentre = new Vector3(0.5f * Screen.width, 0.5f * Screen.height);
+        var boundaryPadding = 20f;
+        screenBounds = new Rect(boundaryPadding, boundaryPadding, Screen.width - 2f * boundaryPadding, Screen.height - 2f * boundaryPadding);
+        shiftable = GetComponent<Shiftable>();
+    }
+
+    public override Image CreateInstance()
+    {
+        var trackerObj = new GameObject(string.Format("{0}_Tracker", transform.name));
+        var trackerImg = trackerObj.AddComponent<Image>();
+
+        trackerImg.rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        trackerImg.color = new Color(1f, 1f, 1f, 1f);
+
+        trackerSprite = Sprite.Create(TrackerCursorImage, new Rect(0, 0, TrackerCursorImage.width, TrackerCursorImage.height), Vector2.zero);
+        arrowSprite = Sprite.Create(ArrowCursorImage, new Rect(0, 0, ArrowCursorImage.width, ArrowCursorImage.height), Vector2.zero);
+
+        trackerImg.sprite = trackerSprite;
+        trackerImg.SetNativeSize();
+
+        var labelObject = new GameObject(string.Format("{0}_Label", transform.name));
+        var labelText = labelObject.AddComponent<Text>();
+
+        labelText.font = LabelFont;
+        labelText.fontSize = 10;
+        labelText.rectTransform.pivot = new Vector2(0.5f, 0.9f);
+        labelText.color = new Color(1f, 1f, 1f, 1f);
+        labelText.alignment = TextAnchor.MiddleCenter;
+        labelText.rectTransform.localScale = Vector3.one;
+
+        labelObject.transform.SetParent(trackerObj.transform);
+
+        labelInstance = labelText;
+        imageInstance = trackerImg;
+        isVisible = false;
+        TriggerFadeIn();
+
+        return trackerImg;
+    }
+
+    public override void UpdateInstance()
+    {
+        var screenPosition = Universe.Current.ViewPort.AttachedCamera.WorldToScreenPoint(transform.position);
+        if (screenPosition.z < 0f)
+        {
+            screenPosition *= -1f;
+            screenPosition = (screenPosition - new Vector3(screenCentre.x, screenCentre.y, 0f))*Utility.ProjectOffscreenLength + new Vector3(screenCentre.x, screenCentre.y, 0f);
+        }
+        screenPosition.z = 0f;
+
+        if (screenBounds.Contains(screenPosition))
+        {
+            imageInstance.sprite = trackerSprite;
+            imageInstance.rectTransform.localPosition = screenPosition - new Vector3(screenCentre.x, screenCentre.y, 0f);
+            imageInstance.rectTransform.localRotation = Quaternion.identity;
+
+            if (PlayerController.Current.VehicleInstance != null)
+            {
+                var fromPlayer = shiftable.GetAbsoluteUniversePosition() - PlayerController.Current.VehicleInstance.Shiftable.GetAbsoluteUniversePosition();
+                labelInstance.text = DistanceDisplay.GetDistanceString(fromPlayer.magnitude);
+                labelInstance.enabled = true;
+            }
+        }
+        else
+        {
+            imageInstance.sprite = arrowSprite;
+            imageInstance.rectTransform.localPosition = Utility.GetBoundsIntersection(screenPosition, screenBounds);
+            imageInstance.rectTransform.localRotation = Quaternion.Euler(0f, 0f, GetScreenAngle(screenPosition));
+
+            labelInstance.enabled = false;
+        }
+    }
+
+    public void TriggerFadeIn(float time = 0.5f)
+    {
+        if (!isFading && !isVisible)
+        {
+            StartCoroutine(FadeIn(time));
+        }
+    }
+
+    public void TriggerFadeOut(float time, Action onFinish)
+    {
+        if (!isFading && isVisible)
+        {
+            StartCoroutine(FadeOut(time, onFinish));
+        }
+    }
+
+    private float fadeStep = 0.02f;
+
+    private IEnumerator FadeOut(float duration, Action onFinish = null)
+    {
+        var stepFraction = fadeStep / duration;
+        for (var fraction = 1f; fraction >= 0f; fraction -= stepFraction)
+        {
+            imageInstance.color = new Color(1f, 1f, 1f, fraction);
+            yield return new WaitForSeconds(fadeStep);
+        }
+        imageInstance.color = new Color(1f, 1f, 1f, 0f);
+        isVisible = false;
+        isFading = false;
+        if (onFinish != null)
+            onFinish();
+    }
+
+    private IEnumerator FadeIn(float duration)
+    {
+        var stepFraction = fadeStep / duration;
+        for (var fraction = 1f; fraction >= 0f; fraction -= stepFraction)
+        {
+            imageInstance.color = new Color(1f, 1f, 1f, 1f - fraction);
+            yield return new WaitForSeconds(fadeStep);
+        }
+        imageInstance.color = new Color(1f, 1f, 1f, 1f);
+        isVisible = true;
+        isFading = false;
+    }
+
+    public void SelfDestroy()
+    {
+        TriggerFadeOut(0.5f, () =>
+        {
+            Destroy(this);
+        });
+    }
+
+    public override void DestroyInstance()
+    {
+        if (imageInstance != null)
+            Destroy(imageInstance);
+        if (labelInstance != null)
+            Destroy(labelInstance);
+    }
+
+    public override void SetVisible(bool value)
+    {
+        imageInstance.enabled = value;
+        labelInstance.enabled = value;
+    }
+
+    private float GetScreenAngle(Vector2 point)
+    {
+        var delta = point - screenCentre;
+        var angle = Mathf.Rad2Deg * Mathf.Atan2(delta.x, -delta.y) + 180f;
+        return angle;
+    }
+}
