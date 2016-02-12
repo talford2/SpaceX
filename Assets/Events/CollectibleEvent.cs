5188266e08a92001e197061190facdd280e76042
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-13 00:19:25 +0800

    Create collectible event.

---
+++ Assets/Events/CollectibleEvent.cs
@@ -0,0 +1,21 @@
+﻿public class CollectibleEvent : UniverseEvent
+{
+    public Collectible CollectiblePrefab;
+
+    private MapPin _mapPin;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _mapPin = GetComponent<MapPin>();
+    }
+
+    public override void Trigger()
+    {
+        if (_mapPin != null)
+            _mapPin.SetPinState(MapPin.MapPinState.Inactive);
+        var collectible = Instantiate(CollectiblePrefab);
+        collectible.Shiftable.SetShiftPosition(Shiftable.UniversePosition);
+        base.Trigger();
+    }
+}
