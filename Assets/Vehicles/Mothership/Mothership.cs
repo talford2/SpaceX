3076bfa5a626250d615493e003b3ba0233825256
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-13 15:54:14 +0800

    Some progress.

---
+++ Assets/Vehicles/Mothership/Mothership.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Mothership : MonoBehaviour
+{
+    public GameObject TurretPrefab;
+    public List<Transform> TurretTransforms;
+
+    private void Start()
+    {
+        foreach (var turretTransform in TurretTransforms)
+        {
+            var turret = (GameObject)Instantiate(TurretPrefab, turretTransform.position, turretTransform.rotation);
+            turret.transform.SetParent(turretTransform);
+        }
+    }
+}
