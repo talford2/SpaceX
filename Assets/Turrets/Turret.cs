208cf5d5b5eb92a62800d788fea795cd2fe8b44a
Timothy <talford2@hotmail.com>
2016-02-13 19:32:45 +1100

    Fixing the turret tracker business

--- Assets/Turrets/Turret.cs
+++ Assets/Turrets/Turret.cs
@@ -3,143 +3,144 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    [Header("Parts")]
-    public GameObject Head;
-    public GameObject Guns;
+	[Header("Parts")]
+	public GameObject Head;
+	public GameObject Guns;
 
-    public Weapon WeaponPrefab;
-    public List<ShootPoint> ShootPoints;
-    public float MaxTargetDistance;
+	public Weapon WeaponPrefab;
+	public List<ShootPoint> ShootPoints;
+	public float MaxTargetDistance;
 
-    [Header("Turning")]
-    public float MinPitch;
-    public float MaxPitch;
-    public float MaxPitchSpeed = 90f;
-    public float MaxYawSpeed = 90f;
+	[Header("Turning")]
+	public float MinPitch;
+	public float MaxPitch;
+	public float MaxPitchSpeed = 90f;
+	public float MaxYawSpeed = 90f;
 
-    [Header("Aiming")]
-    public float AimTolerance = 5f;
-    public float ExtrapolationTimeError = 0.5f;
+	[Header("Aiming")]
+	public float AimTolerance = 5f;
+	public float ExtrapolationTimeError = 0.5f;
+	public Targetable Targetable;
 
-    private Targetable _targetable;
-    private VelocityReference _velocityReference;
-    private Weapon _weaponInstance;
+	private VelocityReference _velocityReference;
+	private Weapon _weaponInstance;
 
-    private Transform _target;
-    private float targetSearchInterval = 2f;
-    private float targetSearchCooldown;
+	private Transform _target;
+	private float _targetSearchInterval = 2f;
+	private float _targetSearchCooldown;
 
+	private float _aimInterval = 0.3f;
+	private float _aimCooldown;
+	private Vector3 _aimPosition;
 
-    private float aimInterval = 0.3f;
-    private float aimCooldown;
-    private Vector3 aimPosition;
+	private float _yaw;
+	private float _pitch;
 
-    private float _yaw;
-    private float _pitch;
+	private void Awake()
+	{
+		foreach (var shootPoint in ShootPoints)
+		{
+			shootPoint.Initialize();
+		}
 
-    private void Awake()
-    {
-        foreach (var shootPoint in ShootPoints)
-        {
-            shootPoint.Initialize();
-        }
+		_velocityReference = new VelocityReference(Vector3.zero);
+		if (Targetable == null)
+		{
+			Targetable = GetComponent<Targetable>();
+		}
 
-        _velocityReference = new VelocityReference(Vector3.zero);
-        _targetable = GetComponent<Targetable>();
+		_weaponInstance = Utility.InstantiateInParent(WeaponPrefab.gameObject, transform).GetComponent<Weapon>();
+		_weaponInstance.Initialize(gameObject, ShootPoints, _velocityReference, Targetable.Team);
+	}
 
-        _weaponInstance = Utility.InstantiateInParent(WeaponPrefab.gameObject, transform).GetComponent<Weapon>();
-        _weaponInstance.Initialize(gameObject, ShootPoints, _velocityReference, _targetable.Team);
-    }
+	private void Update()
+	{
+		var shootPointsCentre = _weaponInstance.GetShootPointCentre();
 
-    private void Update()
-    {
-        var shootPointsCentre = _weaponInstance.GetShootPointCentre();
+		if (_targetSearchCooldown >= 0f)
+		{
+			_targetSearchCooldown -= Time.deltaTime;
+			if (_targetSearchCooldown < 0f)
+			{
+				_target = Targeting.FindFacingAngleTeam(Targeting.GetEnemyTeam(Targetable.Team), transform.position, Guns.transform.forward, MaxTargetDistance);
+				if (_target == null)
+					_target = Targeting.FindNearestTeam(Targeting.GetEnemyTeam(Targetable.Team), transform.position, MaxTargetDistance);
+				_targetSearchCooldown = _targetSearchInterval;
+				_aimCooldown = 0f;
+			}
+		}
 
-        if (targetSearchCooldown >= 0f)
-        {
-            targetSearchCooldown -= Time.deltaTime;
-            if (targetSearchCooldown < 0f)
-            {
-                _target = Targeting.FindFacingAngleTeam(Targeting.GetEnemyTeam(_targetable.Team), transform.position, Guns.transform.forward, MaxTargetDistance);
-                if (_target == null)
-                    _target = Targeting.FindNearestTeam(Targeting.GetEnemyTeam(_targetable.Team), transform.position, MaxTargetDistance);
-                targetSearchCooldown = targetSearchInterval;
-                aimCooldown = 0f;
-            }
-        }
+		if (_target != null)
+		{
+			if (_aimCooldown >= 0f)
+			{
+				_aimCooldown -= Time.deltaTime;
+				if (_aimCooldown < 0f)
+				{
+					_aimPosition = _target.transform.position;
 
-        if (_target != null)
-        {
-            if (aimCooldown >= 0f)
-            {
-                aimCooldown -= Time.deltaTime;
-                if (aimCooldown < 0f)
-                {
-                    aimPosition = _target.transform.position;
+					var targetVehicle = _target.GetComponent<Vehicle>();
+					if (targetVehicle != null)
+						_aimPosition = Utility.GetVehicleExtrapolatedPosition(targetVehicle, _weaponInstance, 0f); //Random.Range(-ExtrapolationTimeError, ExtrapolationTimeError));
 
-                    var targetVehicle = _target.GetComponent<Vehicle>();
-                    if (targetVehicle != null)
-                        aimPosition = Utility.GetVehicleExtrapolatedPosition(targetVehicle, _weaponInstance, 0f); //Random.Range(-ExtrapolationTimeError, ExtrapolationTimeError));
+					_aimCooldown = _aimInterval;
+				}
+			}
 
-                    aimCooldown = aimInterval;
-                }
-            }
+			Head.transform.LookAt(_aimPosition, Vector3.up);
+			var targetYaw = Head.transform.localEulerAngles.y;
+			//var targetYaw = Quaternion.LookRotation(aimPosition - Head.transform.position, Vector3.up).eulerAngles.y; // this is world space not local!
 
-            Head.transform.LookAt(aimPosition, Vector3.up);
-            var targetYaw = Head.transform.localEulerAngles.y;
-            //var targetYaw = Quaternion.LookRotation(aimPosition - Head.transform.position, Vector3.up).eulerAngles.y; // this is world space not local!
+			_yaw = Mathf.MoveTowardsAngle(_yaw, targetYaw, MaxYawSpeed * Time.deltaTime);
+			Head.transform.localRotation = Quaternion.Euler(0, _yaw, 0);
 
-            _yaw = Mathf.MoveTowardsAngle(_yaw, targetYaw, MaxYawSpeed*Time.deltaTime);
-            Head.transform.localRotation = Quaternion.Euler(0, _yaw, 0);
+			Guns.transform.LookAt(_aimPosition, Vector3.forward);
+			var targetPitch = Guns.transform.localEulerAngles.x;
+			//var targetPitch = Quaternion.LookRotation(aimPosition - Guns.transform.position, Vector3.forward).eulerAngles.x; // this is world space not local!
 
-            Guns.transform.LookAt(aimPosition, Vector3.forward);
-            var targetPitch = Guns.transform.localEulerAngles.x;
-            //var targetPitch = Quaternion.LookRotation(aimPosition - Guns.transform.position, Vector3.forward).eulerAngles.x; // this is world space not local!
+			var deltaMin = Mathf.DeltaAngle(targetPitch, MinPitch);
+			var deltaMax = Mathf.DeltaAngle(targetPitch, MaxPitch);
 
-            var deltaMin = Mathf.DeltaAngle(targetPitch, MinPitch);
-            var deltaMax = Mathf.DeltaAngle(targetPitch, MaxPitch);
+			if (deltaMin < 0f)
+			{
+				// Lower Limit
+				targetPitch = MinPitch;
+			}
+			else
+			{
+				if (deltaMax > 0f)
+				{
+					// Upper Limit
+					targetPitch = MaxPitch;
+				}
+			}
+			_pitch = Mathf.MoveTowardsAngle(_pitch, targetPitch, MaxPitchSpeed * Time.deltaTime);
+			Guns.transform.localRotation = Quaternion.Euler(targetPitch, 0, 0);
 
-            if (deltaMin < 0f)
-            {
-                // Lower Limit
-                targetPitch = MinPitch;
-            }
-            else
-            {
-                if (deltaMax > 0f)
-                {
-                    // Upper Limit
-                    targetPitch = MaxPitch;
-                }
-            }
-            _pitch = Mathf.MoveTowardsAngle(_pitch, targetPitch, MaxPitchSpeed*Time.deltaTime);
-            Guns.transform.localRotation = Quaternion.Euler(targetPitch, 0, 0);
-
-            // Shooting
-            var toTarget = _target.position - shootPointsCentre;
-            var angleTo = Vector3.Angle(Guns.transform.forward, toTarget);
-            if (Mathf.Abs(angleTo) < AimTolerance)
-            {
-                _weaponInstance.SetAimAt(shootPointsCentre + Guns.transform.forward * MaxTargetDistance);
-                RaycastHit aimHit;
-                if (Physics.Raycast(new Ray(shootPointsCentre, Guns.transform.forward), out aimHit, MaxTargetDistance))
-                {
-                    var aimAtTargetable = aimHit.collider.GetComponentInParent<Targetable>();
-                    if (aimAtTargetable != null)
-                    {
-                        if (_targetable.Team == aimAtTargetable.Team)
-                        {
-                            _weaponInstance.IsTriggered = false;
-                        }
-                    }
-                }
-                _weaponInstance.IsTriggered = true;
-            }
-        }
-        else
-        {
-            _weaponInstance.IsTriggered = false;
-        }
-    }
-
+			// Shooting
+			var toTarget = _target.position - shootPointsCentre;
+			var angleTo = Vector3.Angle(Guns.transform.forward, toTarget);
+			if (Mathf.Abs(angleTo) < AimTolerance)
+			{
+				_weaponInstance.SetAimAt(shootPointsCentre + Guns.transform.forward * MaxTargetDistance);
+				RaycastHit aimHit;
+				if (Physics.Raycast(new Ray(shootPointsCentre, Guns.transform.forward), out aimHit, MaxTargetDistance))
+				{
+					var aimAtTargetable = aimHit.collider.GetComponentInParent<Targetable>();
+					if (aimAtTargetable != null)
+					{
+						if (Targetable.Team == aimAtTargetable.Team)
+						{
+							_weaponInstance.IsTriggered = false;
+						}
+					}
+				}
+				_weaponInstance.IsTriggered = true;
+			}
+		}
+		else
+		{
+			_weaponInstance.IsTriggered = false;
+		}
+	}
 }
