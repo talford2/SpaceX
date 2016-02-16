d78d83d8851f9bf6af86b68e4f6e598aaa77dad3
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-16 15:25:58 +0800

    Towards functional motherships.

---
+++ Assets/Scripts/ProximitySpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ProximitySpawner : MonoBehaviour
+{
+    public Fighter FighterPrefab;
+    public float Radius = 2000f;
+    public float Interval = 20f;
+    public Shiftable Shiftable;
+
+    private float _radiusSquared;
+    private float _intervalCooldown;
+
+    private void Awake()
+    {
+        _radiusSquared = Radius*Radius;
+        _intervalCooldown = Random.Range(0f, Interval);
+    }
+
+    private void Update()
+    {
+        if (PlayerController.Current != null && PlayerController.Current.VehicleInstance != null)
+        {
+            if (_intervalCooldown >= 0f)
+            {
+                _intervalCooldown -= Time.deltaTime;
+                if (_intervalCooldown < 0f)
+                {
+                    var toPlayer = PlayerController.Current.VehicleInstance.transform.position - transform.position;
+                    if (toPlayer.sqrMagnitude < _radiusSquared)
+                    {
+                        Spawn();
+                    }
+                    _intervalCooldown = Interval;
+                }
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        var fighterInst = Instantiate(FighterPrefab);
+        fighterInst.SpawnVehicle(fighterInst.VehiclePrefab, Universe.Current.GetUniversePosition(transform.position), transform.rotation);
+    }
+}
---
+++ Assets/Vehicles/Mothership/MothershipBay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MothershipBay : MonoBehaviour
+{
+    public ProximitySpawner AttachedSpanwer;
+
+    private void OnDestroy()
+    {
+        Destroy(AttachedSpanwer.gameObject);
+    }
+}
