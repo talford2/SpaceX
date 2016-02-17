eb64c3a9a82048ee1e199e60d8ab4dfb29d60ea8
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-17 18:16:07 +0800

    Further tweaking of mothership.

--- Assets/Scripts/ProximitySpawner.cs
+++ Assets/Scripts/ProximitySpawner.cs
@@ -5,15 +5,19 @@ public class ProximitySpawner : MonoBehaviour
     public Fighter FighterPrefab;
     public float Radius = 2000f;
     public float Interval = 20f;
+    public int MaxLiveCount = 2;
     public Transform PathPoint;
 
     private float _radiusSquared;
     private float _intervalCooldown;
 
+    private int _liveCount;
+
     private void Awake()
     {
         _radiusSquared = Radius*Radius;
         _intervalCooldown = Random.Range(0f, Interval);
+        _liveCount = 0;
     }
 
     private void Update()
@@ -28,7 +32,11 @@ public class ProximitySpawner : MonoBehaviour
                     var toPlayer = PlayerController.Current.VehicleInstance.transform.position - transform.position;
                     if (toPlayer.sqrMagnitude < _radiusSquared)
                     {
-                        Spawn();
+                        if (_liveCount < MaxLiveCount)
+                        {
+                            _liveCount++;
+                            Spawn();
+                        }
                     }
                     _intervalCooldown = Interval;
                 }
@@ -40,9 +48,17 @@ public class ProximitySpawner : MonoBehaviour
     {
         var fighterInst = Instantiate(FighterPrefab);
         fighterInst.SpawnVehicle(fighterInst.VehiclePrefab, Universe.Current.GetUniversePosition(transform.position), transform.rotation);
+        var spawnedKillable = fighterInst.VehicleInstance.GetComponent<Killable>();
+        spawnedKillable.OnDie += OnSpawnedDie;
         if (PathPoint != null)
         {
             fighterInst.SetPath(Universe.Current.GetUniversePosition(PathPoint.position));
         }
+    }
+
+    private void OnSpawnedDie(Killable sender)
+    {
+        _liveCount--;
+        Debug.Log("SPAWNED DIED!");
     }
 }
