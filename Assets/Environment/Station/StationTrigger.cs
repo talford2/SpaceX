9195217b01faa94365567cb17427265b98c13487
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-21 01:03:09 +0800

    Rebuild space station and trigger hangar.

---
+++ Assets/Environment/Station/StationTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StationTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var detectable = other.GetComponent<Detectable>();
+        if (detectable != null)
+        {
+            if (PlayerController.Current.VehicleInstance != null)
+            {
+                if (PlayerController.Current.VehicleInstance.transform == detectable.TargetTransform)
+                {
+                    ShipProfileScreen.Current.Fighters.Clear();
+                    foreach (var member in PlayerController.Current.Squadron.Members)
+                    {
+                        ShipProfileScreen.Current.Fighters.Add(member);
+                    }
+                    ShipProfileScreen.Current.Populate(PlayerController.Current.Squadron.GetCurrentIndex());
+                    ShipProfileScreen.Current.Show();
+                }
+            }
+        }
+    }
+}
