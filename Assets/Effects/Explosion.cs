01360f1ebcf907379f83c3d62bbaf0f06bad0f91
Timothy <talford2@hotmail.com>
2016-02-22 01:55:03 +1100

    Adding explosion script for reuse

---
+++ Assets/Effects/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+	private Shiftable _shiftable;
+
+	public AudioSource ExplosionSound;
+
+	void Awake()
+	{
+		_shiftable = GetComponent<Shiftable>();
+		foreach (var ps in _shiftable.ShiftParticleSystems)
+		{
+			ps.Stop();
+		}
+	}
+
+	public void Reset()
+	{
+		foreach (var ps in _shiftable.ShiftParticleSystems)
+		{
+			ps.Play();
+			ExplosionSound.Play();
+		}
+	}
+}
