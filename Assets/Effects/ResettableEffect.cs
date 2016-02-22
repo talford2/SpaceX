7cee49b04fb02a92d860d9e3f3dae5d01c5366db
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-22 18:28:06 +0800

    Replace explosion script with ResettableEffect.

---
+++ Assets/Effects/ResettableEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ResettableEffect : MonoBehaviour {
+    private Shiftable _shiftable;
+
+    public AudioSource Sound;
+
+    void Awake()
+    {
+        _shiftable = GetComponent<Shiftable>();
+        foreach (var ps in _shiftable.ShiftParticleSystems)
+        {
+            ps.Stop();
+        }
+    }
+
+    void Start()
+    {
+        var poolItem = GetComponent<ResourcePoolItem>();
+        if (poolItem != null)
+        {
+            poolItem.OnGetAvaiable = () =>
+            {
+                Reset();
+            };
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (var ps in _shiftable.ShiftParticleSystems)
+        {
+            ps.Play();
+        }
+        Sound.Play();
+    }
+}
