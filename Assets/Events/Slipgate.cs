edcfa130e58c1d9c0a56de7a91a5baa94e0a8b9d
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-22 21:01:25 +0800

    Improve Slipgate.

--- Assets/Events/Slipgate.cs
+++ Assets/Events/Slipgate.cs
@@ -3,16 +3,26 @@
 public class Slipgate : MonoBehaviour
 {
 	public int LevelIndex = 0;
+    public float TriggerRadius = 20f;
 
 	private bool _isTriggered = false;
+    private float _lastDotProd;
 
     void Update()
     {
-        if (!_isTriggered && PlayerController.Current.VehicleInstance != null && (PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position).sqrMagnitude < 9)
+        if (!_isTriggered && PlayerController.Current.VehicleInstance != null)
         {
-            Debug.Log("WARP!");
-            _isTriggered = true;
-            LevelManager.Current.ChangeLevel(LevelIndex);
+            var dotProd = Vector3.Dot(PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position, transform.forward);
+            if ((PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position).sqrMagnitude < TriggerRadius * TriggerRadius)
+            {
+                if (Mathf.Sign(dotProd) != Mathf.Sign(_lastDotProd))
+                {
+                    Debug.Log("WARP!");
+                    _isTriggered = true;
+                    LevelManager.Current.ChangeLevel(LevelIndex);
+                }
+            }
+            _lastDotProd = dotProd;
         }
     }
 }
