ce0838d0a305d2a2ae8841de07af238ecb171844
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-28 03:58:35 +0800

    Hide plasma on stop.

---
+++ Assets/Weapons/Missiles/Plasma.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plasma : Laser {
+    [Header("Plasma")]
+    public MeshRenderer PlasmaMesh;
+
+    public override void Shoot(Vector3 shootFrom, Vector3 direction, Vector3 initVelocity)
+    {
+        base.Shoot(shootFrom, direction, initVelocity);
+        PlasmaMesh.enabled = true;
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+        PlasmaMesh.enabled = false;
+    }
+}
