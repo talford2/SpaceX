e8d19bcaa6682bb0c19d4ee534eec4025ad0dde1
Steven Alford <steven.alford@collinsonlatitude.com>
2016-05-18 21:58:56 +0800

    Add loading screen.

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Text LoadingText;
+
+    private bool isLoading;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene("Test1"));
+    }
+
+    private void Update()
+    {
+        LoadingText.color = Utility.SetColorAlpha(LoadingText.color, Mathf.PingPong(Time.time, 1f));
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        var async = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!async.isDone)
+        {
+            isLoading = false;
+            yield return null;
+        }
+    }
+}
