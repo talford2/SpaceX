9654f410b595f5565e4f8ab81ff3b4fa3e7afca8
Steven Alford <steven.alford@collinsonlatitude.com>
2016-05-22 02:33:49 +0800

    Destructible containers on freighter.

---
+++ Assets/Vehicles/Freighter/Freighter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Freighter : MonoBehaviour
+{
+    public List<Transform> Containers;
+    public float ContainerHealth = 50f;
+
+    [Header("Container Drop Items")]
+    public GameObject DropItemPrefab;
+    public int MinDropCount = 3;
+    public int MaxDropCount = 10;
+
+    private bool hasBeenShot;
+
+    void Start()
+    {
+        foreach(var container in Containers)
+        {
+            var killable = container.GetComponent<Killable>();
+            if (killable!=null)
+            {
+                killable.MaxHealth = ContainerHealth;
+                killable.Health = killable.MaxHealth;
+                killable.OnDamage += OnDamage;
+                killable.OnDie += OnContainerDie;
+            }
+        }
+        hasBeenShot = false;
+    }
+
+    private void OnDamage(Killable sender, Vector3 position, Vector3 normal, GameObject attacker)
+    {
+        if (!hasBeenShot)
+        {
+            hasBeenShot = true;
+            // Trigger enemies
+        }
+    }
+
+    private void OnContainerDie(Killable sender)
+    {
+        var dropAmount = Random.Range(MinDropCount, MaxDropCount + 1);
+        for (var i = 0f; i < dropAmount; i++)
+        {
+            var dropPosition = sender.transform.position + Random.onUnitSphere * 1.5f;
+            var collectible = DropItemPrefab;
+            var dropItem = ((GameObject)Instantiate(collectible, dropPosition, Quaternion.identity)).GetComponent<Collectible>();
+            dropItem.Shiftable.SetShiftPosition(Universe.Current.GetUniversePosition(dropPosition));
+            dropItem.SetVelocity(Random.onUnitSphere * 5f);
+        }
+    }
+}
