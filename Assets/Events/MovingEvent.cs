e25ea747b321e24bcaa063e652d4b6834a1f3fa7
Steven Alford <steven.alford@collinsonlatitude.com>
2016-05-22 04:06:26 +0800

    Add cruise movement to freighter.

---
+++ Assets/Events/MovingEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MovingEvent : MonoBehaviour
+{
+    public Shiftable Shiftable;
+    public float Speed = 10f;
+
+    private void Update()
+    {
+        Shiftable.Translate(Speed * transform.forward * Time.deltaTime);
+    }
+}
