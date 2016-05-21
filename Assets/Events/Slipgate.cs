31d24f5ac00d17a38602834c316689d53de6a296
T <talford2@hotmail.com>
2016-05-21 16:16:30 +1000

    Adding tracker idea

--- Assets/Events/Slipgate.cs
+++ Assets/Events/Slipgate.cs
@@ -3,32 +3,33 @@
 public class Slipgate : MonoBehaviour
 {
 	public int LevelIndex = 0;
-    public float TriggerRadius = 20f;
+	public float TriggerRadius = 20f;
 
 	private bool _isTriggered = false;
-    private float _lastDotProd;
+	private float _lastDotProd;
 
-    void Update()
-    {
-        if (!_isTriggered && PlayerController.Current.VehicleInstance != null)
-        {
-            var dotProd = Vector3.Dot(PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position, transform.forward);
-            if ((PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position).sqrMagnitude < TriggerRadius * TriggerRadius)
-            {
-                if (Mathf.Sign(dotProd) != Mathf.Sign(_lastDotProd))
-                {
-                    Debug.Log("WARP!");
-                    _isTriggered = true;
-                    LevelManager.Current.ChangeLevel(LevelIndex);
-                }
-            }
-            _lastDotProd = dotProd;
-        }
-    }
+	void Update()
+	{
+		if (!_isTriggered && PlayerController.Current.VehicleInstance != null)
+		{
+			var dotProd = Vector3.Dot(PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position, transform.forward);
+			if ((PlayerController.Current.VehicleInstance.Shiftable.GetWorldPosition() - transform.position).sqrMagnitude < TriggerRadius * TriggerRadius)
+			{
+				if (Mathf.Sign(dotProd) != Mathf.Sign(_lastDotProd))
+				{
+					Debug.Log("WARP!");
+					_isTriggered = true;
+					LevelManager.Current.ChangeLevel(LevelIndex);
+					LevelIndex += 1;
+				}
+			}
+			_lastDotProd = dotProd;
+		}
+	}
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, TriggerRadius);
-    }
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, TriggerRadius);
+	}
 }
