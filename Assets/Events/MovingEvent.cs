1172df91b5966f29de99e28aa12c90acbc875a80
Bodedoctor <alford.steven@gmail.com>
2016-06-22 20:51:36 +0800

    Add map pin to freighter.

--- Assets/Events/MovingEvent.cs
+++ Assets/Events/MovingEvent.cs
@@ -5,8 +5,24 @@ public class MovingEvent : MonoBehaviour
     public Shiftable Shiftable;
     public float Speed = 10f;
 
+    public bool DestroyOverDistance;
+    public float DestroyDistance;
+
+    private float destroyDistSquared;
+
+    private void Awake()
+    {
+        destroyDistSquared = DestroyDistance * DestroyDistance;
+    }
+
     private void Update()
     {
+        var distSquared = Shiftable.GetAbsoluteUniversePosition().sqrMagnitude;
+        if (distSquared > destroyDistSquared)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Shiftable.Translate(Speed * transform.forward * Time.deltaTime);
     }
 }
