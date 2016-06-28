d37dc9b6ab343ac10a547d0edd15a54c9e6edbaf
Bodedoctor <alford.steven@gmail.com>
2016-06-28 17:40:51 +0800

    Increase distant scaling threshold distance.

--- Assets/Scripts/DistantScaling.cs
+++ Assets/Scripts/DistantScaling.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class DistantScaling : MonoBehaviour
 {
-	private float _thresholdDistance;
 	private float _thresholdDistanceSquared;
 	private float _distantScale;
 
@@ -15,15 +14,14 @@ public class DistantScaling : MonoBehaviour
     private bool _isDistant;
     private bool _lastIsDistant;
 
+    public float ThresholdDistance = 2000f;
     public GameObject DistantObject;
     public GameObject NearObject;
 
 	private void Awake()
 	{
-		_thresholdDistance = 1000f;
 		_distantScale = 1000f;
-
-		_thresholdDistanceSquared = _thresholdDistance * _thresholdDistance;
+		_thresholdDistanceSquared = ThresholdDistance * ThresholdDistance;
 	}
 
 	private void Start()
@@ -51,7 +49,7 @@ public class DistantScaling : MonoBehaviour
 
 			// This will only be able to scale objects of a radius up to the _thresholdDistance.
 			// Keep the scaled object 1 unit in front of the camera!
-			var scaledDistance = Mathf.Abs(distance - _thresholdDistance - 1f) / _distantScale + 1f; // + _thresholdDistance;
+			var scaledDistance = Mathf.Abs(distance - ThresholdDistance - 1f) / _distantScale + 1f; // + _thresholdDistance;
 
 			var scale = Mathf.Clamp(scaledDistance / distance, 0f, 1f);
 			transform.localScale = new Vector3(scale, scale, scale);
