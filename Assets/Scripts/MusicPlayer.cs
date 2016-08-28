9396809b3210ea89fe14c84226f65b5296f2d134
T <talford2@hotmail.com>
2016-08-28 23:48:18 +1000

    Music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public List<AudioSource> MusicList;
+    private int _currentIndex = 0;
+
+    void Start()
+    {
+        MusicList.First().Play();
+        _currentIndex = 0;
+    }
+
+    void Update()
+    {
+        if (!MusicList[_currentIndex].isPlaying)
+        {
+            _currentIndex++;
+            if (_currentIndex >= MusicList.Count)
+            {
+                _currentIndex = 0;
+            }
+            MusicList[_currentIndex].Play();
+        }
+    }
+}
