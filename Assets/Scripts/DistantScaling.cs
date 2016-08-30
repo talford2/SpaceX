1c7bde03f8afdb7665d5a40ca7f150d7dc38fe87
T <talford2@hotmail.com>
2016-08-30 23:45:27 +1000

    Fog stuff

--- Assets/Scripts/DistantScaling.cs
+++ Assets/Scripts/DistantScaling.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class DistantScaling : MonoBehaviour
 {
-	private float _thresholdDistanceSquared;
-	private float _distantScale;
+    private float _thresholdDistanceSquared;
+    private float _distantScale;
 
-	// Crappy solution
+    // Crappy solution
     public Shiftable FocusShiftable;
-	public float LineRendererWidth = 35f;
-	public List<LineRenderer> LineRenderers;
+    public float LineRendererWidth = 35f;
+    public List<LineRenderer> LineRenderers;
 
     private bool _isDistant;
     private bool _lastIsDistant;
@@ -18,70 +18,79 @@ public class DistantScaling : MonoBehaviour
     public GameObject DistantObject;
     public GameObject NearObject;
 
-	private void Awake()
-	{
-		_distantScale = 1000f;
-		_thresholdDistanceSquared = ThresholdDistance * ThresholdDistance;
-	}
+    public List<Renderer> FogParts;
+    public float MinFogDistance = 2000f;
+    public float MaxFogDistance = 20000f;
 
-	private void Start()
-	{
-		Universe.Current.ViewPort.OnMove += UpdatePositionAndScale;
-	}
+    private void Awake()
+    {
+        _distantScale = 1000f;
+        _thresholdDistanceSquared = ThresholdDistance * ThresholdDistance;
+    }
 
-	private void UpdatePositionAndScale()
-	{
-	    if (FocusShiftable == null)
-	        return;
-		// Scaling
-		var worldDestination = Universe.Current.GetWorldPosition(FocusShiftable.UniversePosition);
+    private void Start()
+    {
+        Universe.Current.ViewPort.OnMove += UpdatePositionAndScale;
+    }
+
+    private void UpdatePositionAndScale()
+    {
+        if (FocusShiftable == null)
+            return;
+        // Scaling
+        var worldDestination = Universe.Current.GetWorldPosition(FocusShiftable.UniversePosition);
         var toCamera = worldDestination - Universe.Current.ViewPort.AttachedCamera.transform.position;
 
-		if (toCamera.sqrMagnitude > _thresholdDistanceSquared)
-		{
-		    if (!_isDistant)
-		    {
+        if (toCamera.sqrMagnitude > _thresholdDistanceSquared)
+        {
+            if (!_isDistant)
+            {
                 DistantObject.SetActive(true);
                 NearObject.SetActive(false);
-		        _isDistant = true;
-		    }
-		    var distance = toCamera.magnitude;
+                _isDistant = true;
+            }
+            var distance = toCamera.magnitude;
 
-			// This will only be able to scale objects of a radius up to the _thresholdDistance.
-			// Keep the scaled object 1 unit in front of the camera!
-			var scaledDistance = Mathf.Abs(distance - ThresholdDistance - 1f) / _distantScale + 1f; // + _thresholdDistance;
+            // This will only be able to scale objects of a radius up to the _thresholdDistance.
+            // Keep the scaled object 1 unit in front of the camera!
+            var scaledDistance = Mathf.Abs(distance - ThresholdDistance - 1f) / _distantScale + 1f; // + _thresholdDistance;
 
-			var scale = Mathf.Clamp(scaledDistance / distance, 0f, 1f);
-			transform.localScale = new Vector3(scale, scale, scale);
+            var scale = Mathf.Clamp(scaledDistance / distance, 0f, 1f);
+            transform.localScale = new Vector3(scale, scale, scale);
 
-			// Crappy solution
-			var lineRenderWidth = scale * LineRendererWidth;
-			foreach (var lineRenderer in LineRenderers)
-			{
-				lineRenderer.SetWidth(lineRenderWidth, lineRenderWidth);
-			}
+            // Crappy solution
+            var lineRenderWidth = scale * LineRendererWidth;
+            foreach (var lineRenderer in LineRenderers)
+            {
+                lineRenderer.SetWidth(lineRenderWidth, lineRenderWidth);
+            }
 
-			// Positioning
+            // Positioning
             transform.position = Universe.Current.ViewPort.AttachedCamera.transform.position + toCamera.normalized * scaledDistance;
-		}
-		else
-		{
-		    if (_isDistant)
-		    {
+
+
+            // Fog stuff
+            var frac = 1 - Mathf.Clamp((distance - MinFogDistance) / (MaxFogDistance - MinFogDistance), 0, 1);
+            FogParts[0].materials[0].color = Utility.SetColorAlpha(FogParts[0].materials[0].color, frac);
+        }
+        else
+        {
+            if (_isDistant)
+            {
                 DistantObject.SetActive(false);
                 NearObject.SetActive(true);
                 _isDistant = false;
-		    }
-		    transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            transform.localScale = new Vector3(1f, 1f, 1f);
 
-			// Crappy solution
-			foreach (var lineRenderer in LineRenderers)
-			{
-				lineRenderer.SetWidth(LineRendererWidth, LineRendererWidth);
-			}
+            // Crappy solution
+            foreach (var lineRenderer in LineRenderers)
+            {
+                lineRenderer.SetWidth(LineRendererWidth, LineRendererWidth);
+            }
 
-			transform.position = worldDestination;
-		}
-	    _lastIsDistant = _isDistant;
-	}
+            transform.position = worldDestination;
+        }
+        _lastIsDistant = _isDistant;
+    }
 }
