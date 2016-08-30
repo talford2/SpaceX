100102d88550ccab4616f14955b63c734b07621f
Bodedoctor <alford.steven@gmail.com>
2016-08-30 23:48:45 +0800

    Reposition freighters instead of destroying on leaving area.

--- Assets/Events/MovingEvent.cs
+++ Assets/Events/MovingEvent.cs
@@ -5,6 +5,8 @@ public class MovingEvent : MonoBehaviour
     public Shiftable Shiftable;
     public float Speed = 10f;
 
+    public bool IsReposition;
+
     public bool DestroyOverDistance;
     public float DestroyDistance;
 
@@ -20,7 +22,14 @@ public class MovingEvent : MonoBehaviour
         var distSquared = Shiftable.GetAbsoluteUniversePosition().sqrMagnitude;
         if (distSquared > destroyDistSquared)
         {
-            Destroy(gameObject);
+            if (IsReposition)
+            {
+                Shiftable.SetShiftPosition(Universe.Current.GetUniversePosition(transform.position - DestroyDistance * transform.forward));
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
             return;
         }
         Shiftable.Translate(Speed * transform.forward * Time.deltaTime);
