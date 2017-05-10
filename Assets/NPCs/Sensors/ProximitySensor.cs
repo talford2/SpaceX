18fce7e013e221a6fc7e1f705357dd4a0529c66c
Bodedoctor <alford.steven@gmail.com>
2017-05-10 22:12:40 +0800

    Tweak proximity sensor for performance.

--- Assets/NPCs/Sensors/ProximitySensor.cs
+++ Assets/NPCs/Sensors/ProximitySensor.cs
@@ -3,23 +3,27 @@ using UnityEngine;
 
 public class ProximitySensor : MonoBehaviour
 {
-	public float Radius;
+    public float Radius;
 
-	private int _detectableMask;
+    private int _detectableMask;
+    private const int _detectableMaxBuffer = 1024;
+    private Collider[] _detectables;
+    private Detectable _curDetectable;
 
-	public void Awake()
-	{
-		_detectableMask = LayerMask.GetMask("Detectable");
-	}
+    public void Awake()
+    {
+        _detectableMask = LayerMask.GetMask("Detectable");
+        _detectables = new Collider[_detectableMaxBuffer];
+    }
 
-	public void Detect(Action<Transform> action)
-	{
-		var detectables = Physics.OverlapSphere(transform.position, Radius, _detectableMask);
-		foreach (var detected in detectables)
-		{
-			var detectable = detected.GetComponent<Detectable>();
-			if (detectable != null)
-				action(detectable.TargetTransform);
-		}
-	}
+    public void Detect(Action<Transform> action)
+    {
+        var detectableCount = Physics.OverlapSphereNonAlloc(transform.position, Radius, _detectables, _detectableMask);
+        for (var i = 0; i < detectableCount; i++)
+        {
+            _curDetectable = _detectables[i].GetComponent<Detectable>();
+            if (_curDetectable != null)
+                action(_curDetectable.TargetTransform);
+        }
+    }
 }
