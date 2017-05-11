89516331e02086b36ef187d7dbdcd64514a6973f
Bodedoctor <alford.steven@gmail.com>
2017-05-11 20:22:57 +0800

    Reduce inheritance of initial velocity direction for seeking rockets.

--- Assets/Weapons/Missiles/SeekingRocket.cs
+++ Assets/Weapons/Missiles/SeekingRocket.cs
@@ -3,263 +3,263 @@
 [RequireComponent(typeof(Shiftable))]
 public class SeekingRocket : Missile
 {
-	[Header("Seeking Rocket")]
-	public float MinChaseDistance = 50f;
-	public float MaxTurnSpeed = 90f;
-	public float Radius = 0.3f;
-	public GameObject ExplodePrefab;
-	public MeshRenderer Rocket;
-	public MeshRenderer ThrusterMesh;
-	public ShiftTrail Tracer;
-	public FlareDistanceBrightness Flare;
+    [Header("Seeking Rocket")]
+    public float MinChaseDistance = 50f;
+    public float MaxTurnSpeed = 90f;
+    public float Radius = 0.3f;
+    public GameObject ExplodePrefab;
+    public MeshRenderer Rocket;
+    public MeshRenderer ThrusterMesh;
+    public ShiftTrail Tracer;
+    public FlareDistanceBrightness Flare;
 
-	private Shiftable _shiftable;
-	private ResourcePoolItem _resourcePoolItem;
+    private Shiftable _shiftable;
+    private ResourcePoolItem _resourcePoolItem;
 
-	private Transform _target;
-	private Vector3 _shootFrom;
-	private Vector3 _initVelocity;
-	private Vector3 _offsetVelocity;
+    private Transform _target;
+    private Vector3 _shootFrom;
+    private Vector3 _initVelocity;
+    private Vector3 _offsetVelocity;
 
-	private bool _isVehicleTarget;
-	private Vehicle _vehicle;
+    private bool _isVehicleTarget;
+    private Vehicle _vehicle;
 
-	private Vector3 _velocity;
+    private Vector3 _velocity;
 
-	private float _explodeDistance = 2f;
+    private float _explodeDistance = 2f;
 
-	private float _travelStraightTime = 0.5f;
-	private float _travelStraightCooldown;
-	private float _noTargetTime = 5f;
-	private float _noTargetCooldown;
+    private float _travelStraightTime = 0.5f;
+    private float _travelStraightCooldown;
+    private float _noTargetTime = 5f;
+    private float _noTargetCooldown;
 
-	private float _turnTime = 2f;
+    private float _turnTime = 2f;
 
-	// Spherecast hit
-	private bool _hasHit;
-	private Vector3 _hitPosition;
-	private Vector3 _observationPosition;
-	private float _rayCheckMaxDistSquared = 25000000f;
-	private float _stopDistanceSquared = 100000000f;
+    // Spherecast hit
+    private bool _hasHit;
+    private Vector3 _hitPosition;
+    private Vector3 _observationPosition;
+    private float _rayCheckMaxDistSquared = 25000000f;
+    private float _stopDistanceSquared = 100000000f;
 
-	private int _mask;
+    private int _mask;
 
-	private int _detectableMask;
+    private int _detectableMask;
 
-	private void Awake()
-	{
-		_shiftable = GetComponent<Shiftable>();
-		_mask = ~LayerMask.GetMask("Distant", "Universe Background", "Environment");
-		_detectableMask = LayerMask.GetMask("Detectable");
-	}
+    private void Awake()
+    {
+        _shiftable = GetComponent<Shiftable>();
+        _mask = ~LayerMask.GetMask("Distant", "Universe Background", "Environment");
+        _detectableMask = LayerMask.GetMask("Detectable");
+    }
 
-	public override void Initialize(GameObject owner, float damage)
-	{
-		if (_resourcePoolItem == null)
-			_resourcePoolItem = GetComponent<ResourcePoolItem>();
-		base.Initialize(owner, damage);
-	}
+    public override void Initialize(GameObject owner, float damage)
+    {
+        if (_resourcePoolItem == null)
+            _resourcePoolItem = GetComponent<ResourcePoolItem>();
+        base.Initialize(owner, damage);
+    }
 
-	public override void LiveUpdate()
-	{
-		if (_target != null)
-		{
-			if (_travelStraightCooldown >= 0)
-			{
-				_travelStraightCooldown -= Time.deltaTime;
-			}
+    public override void LiveUpdate()
+    {
+        if (_target != null)
+        {
+            if (_travelStraightCooldown >= 0)
+            {
+                _travelStraightCooldown -= Time.deltaTime;
+            }
 
-			var toTarget = _target.position - transform.position;
-			if (_isVehicleTarget)
-			{
-				if (toTarget.sqrMagnitude > MinChaseDistance * MinChaseDistance)
-				{
-					_offsetVelocity = _initVelocity;
-				}
-				else
-				{
-					_offsetVelocity = Vector3.Lerp(_offsetVelocity, _vehicle.GetVelocity(), 5f * Time.deltaTime);
-				}
-			}
-			else
-			{
-				if (toTarget.sqrMagnitude > MinChaseDistance * MinChaseDistance)
-				{
-					_offsetVelocity = _initVelocity;
-				}
-				else
-				{
-					_offsetVelocity = Vector3.Lerp(_offsetVelocity, Vector3.zero, 5f * Time.deltaTime);
-				}
-			}
+            var toTarget = _target.position - transform.position;
+            if (_isVehicleTarget)
+            {
+                if (toTarget.sqrMagnitude > MinChaseDistance * MinChaseDistance)
+                {
+                    _offsetVelocity = _initVelocity;
+                }
+                else
+                {
+                    _offsetVelocity = Vector3.Lerp(_offsetVelocity, _vehicle.GetVelocity(), 5f * Time.deltaTime);
+                }
+            }
+            else
+            {
+                if (toTarget.sqrMagnitude > MinChaseDistance * MinChaseDistance)
+                {
+                    _offsetVelocity = _initVelocity;
+                }
+                else
+                {
+                    _offsetVelocity = Vector3.Lerp(_offsetVelocity, Vector3.zero, 5f * Time.deltaTime);
+                }
+            }
 
-			if (_travelStraightCooldown < _turnTime)
-			{
-				var turnFraction = Mathf.Clamp01(1f - _travelStraightCooldown / _turnTime);
-				var maxTurn = MaxTurnSpeed * turnFraction;
-				transform.forward = Vector3.RotateTowards(transform.forward, toTarget.normalized, maxTurn * Time.deltaTime, 0f);
-			}
+            if (_travelStraightCooldown < _turnTime)
+            {
+                var turnFraction = Mathf.Clamp01(1f - _travelStraightCooldown / _turnTime);
+                var maxTurn = MaxTurnSpeed * turnFraction;
+                transform.forward = Vector3.RotateTowards(transform.forward, toTarget.normalized, maxTurn * Time.deltaTime, 0f);
+            }
 
-			if (toTarget.sqrMagnitude < _explodeDistance * _explodeDistance)
-			{
-				Explode();
-			}
-			_noTargetCooldown = _noTargetTime;
-		}
+            if (toTarget.sqrMagnitude < _explodeDistance * _explodeDistance)
+            {
+                Explode();
+            }
+            _noTargetCooldown = _noTargetTime;
+        }
 
-		if (_noTargetCooldown >= 0f)
-		{
-			_noTargetCooldown -= Time.deltaTime;
-			if (_noTargetCooldown < 0f)
-			{
-				Explode();
-			}
-		}
+        if (_noTargetCooldown >= 0f)
+        {
+            _noTargetCooldown -= Time.deltaTime;
+            if (_noTargetCooldown < 0f)
+            {
+                Explode();
+            }
+        }
 
-		_velocity = _offsetVelocity + transform.forward * MissileSpeed;
+        _velocity = _offsetVelocity + transform.forward * MissileSpeed;
 
-		var displacement = _velocity * Time.deltaTime;
+        var displacement = _velocity * Time.deltaTime;
 
-		// Hit objects
-		if (_hasHit)
-		{
-			Explode();
-			return;
-		}
-		_observationPosition = Universe.Current.ViewPort.transform.position;
-		var toOberverSquared = (transform.position - _observationPosition).sqrMagnitude;
-		if (toOberverSquared < _rayCheckMaxDistSquared)
-		{
-			if (!_hasHit)
-			{
-				var missileRay = new Ray(transform.position, _velocity);
-				RaycastHit missileHit;
-				if (Physics.SphereCast(missileRay, Radius, out missileHit, displacement.magnitude, _mask))
-				{
-					if (missileHit.collider.gameObject != Owner)
-					{
-						var killable = missileHit.collider.GetComponentInParent<Killable>();
-						if (killable != null)
-						{
-							killable.Damage(Damage, missileHit.point, missileHit.normal, Owner);
-						}
-						_hasHit = true;
-						_hitPosition = missileHit.point;
-						PlaceHitEffects(missileHit.point, missileHit.normal, missileHit.collider.gameObject.transform);
-					}
-				}
-			}
-		}
-		else
-		{
-			if (toOberverSquared > _stopDistanceSquared)
-			{
-				Stop();
-			}
-		}
+        // Hit objects
+        if (_hasHit)
+        {
+            Explode();
+            return;
+        }
+        _observationPosition = Universe.Current.ViewPort.transform.position;
+        var toOberverSquared = (transform.position - _observationPosition).sqrMagnitude;
+        if (toOberverSquared < _rayCheckMaxDistSquared)
+        {
+            if (!_hasHit)
+            {
+                var missileRay = new Ray(transform.position, _velocity);
+                RaycastHit missileHit;
+                if (Physics.SphereCast(missileRay, Radius, out missileHit, displacement.magnitude, _mask))
+                {
+                    if (missileHit.collider.gameObject != Owner)
+                    {
+                        var killable = missileHit.collider.GetComponentInParent<Killable>();
+                        if (killable != null)
+                        {
+                            killable.Damage(Damage, missileHit.point, missileHit.normal, Owner);
+                        }
+                        _hasHit = true;
+                        _hitPosition = missileHit.point;
+                        PlaceHitEffects(missileHit.point, missileHit.normal, missileHit.collider.gameObject.transform);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (toOberverSquared > _stopDistanceSquared)
+            {
+                Stop();
+            }
+        }
 
-		_shiftable.Translate(displacement);
-	}
+        _shiftable.Translate(displacement);
+    }
 
-	public override void SetTarget(Transform target)
-	{
-		base.SetTarget(target);
-		_target = target;
+    public override void SetTarget(Transform target)
+    {
+        base.SetTarget(target);
+        _target = target;
 
-		_vehicle = _target.GetComponent<Vehicle>();
-		_isVehicleTarget = _vehicle != null;
-		_resourcePoolItem.IsAvailable = false;
-	}
+        _vehicle = _target.GetComponent<Vehicle>();
+        _isVehicleTarget = _vehicle != null;
+        _resourcePoolItem.IsAvailable = false;
+    }
 
-	public override void Shoot(Vector3 shootFrom, Vector3 direction, Vector3 initVelocity)
-	{
-		base.Shoot(shootFrom, direction, initVelocity);
-		_shootFrom = shootFrom;
-		_initVelocity = initVelocity;
+    public override void Shoot(Vector3 shootFrom, Vector3 direction, Vector3 initVelocity)
+    {
+        base.Shoot(shootFrom, direction, initVelocity);
+        _shootFrom = shootFrom;
+        _initVelocity = initVelocity;// Vector3.Dot(initVelocity.normalized, direction.normalized) * initVelocity.magnitude * FromReference.forward;
 
-		_velocity = _initVelocity;
+        _velocity = _initVelocity;
 
-		transform.position = _shootFrom;
-		transform.forward = direction;
-		transform.position += initVelocity * Time.deltaTime;
+        transform.position = _shootFrom;
+        transform.forward = FromReference.forward;
+        transform.position += initVelocity * Time.deltaTime;
 
-		Rocket.enabled = true;
-		ThrusterMesh.enabled = true;
+        Rocket.enabled = true;
+        ThrusterMesh.enabled = true;
 
-		Tracer.Initialize(transform.position);
-		Flare.SetVisible(true);
+        Tracer.Initialize(transform.position);
+        Flare.SetVisible(true);
 
-		_noTargetCooldown = _noTargetTime;
-		_travelStraightCooldown = _travelStraightTime + _turnTime;
-		_hasHit = false;
+        _noTargetCooldown = _noTargetTime;
+        _travelStraightCooldown = _travelStraightTime + _turnTime;
+        _hasHit = false;
 
-		_resourcePoolItem.IsAvailable = false;
-	}
+        _resourcePoolItem.IsAvailable = false;
+    }
 
-	private Collider[] _damageColliders = new Collider[10];
-	private Detectable _detectable;
-	private Killable _killable;
-	private void Explode()
-	{
-		var explodeInstance = ResourcePoolManager.GetAvailable(ExplodePrefab, transform.position, transform.rotation); //(GameObject)Instantiate(ExplodePrefab, transform.position, transform.rotation);
+    private Collider[] _damageColliders = new Collider[10];
+    private Detectable _detectable;
+    private Killable _killable;
+    private void Explode()
+    {
+        var explodeInstance = ResourcePoolManager.GetAvailable(ExplodePrefab, transform.position, transform.rotation); //(GameObject)Instantiate(ExplodePrefab, transform.position, transform.rotation);
 
-		explodeInstance.transform.localScale = transform.localScale;
-		var explodeShiftable = explodeInstance.GetComponent<Shiftable>();
-		var ownerShiftable = GetComponent<Shiftable>();
+        explodeInstance.transform.localScale = transform.localScale;
+        var explodeShiftable = explodeInstance.GetComponent<Shiftable>();
+        var ownerShiftable = GetComponent<Shiftable>();
 
-		if (_target != null)
-		{
-			var targetable = _target.GetComponent<Targetable>();
-			if (targetable != null)
-			{
-				targetable.LockedOnBy = null;
-				Debug.Log("TURN LOCK OFF!");
-			}
-		}
+        if (_target != null)
+        {
+            var targetable = _target.GetComponent<Targetable>();
+            if (targetable != null)
+            {
+                targetable.LockedOnBy = null;
+                Debug.Log("TURN LOCK OFF!");
+            }
+        }
 
-		if (explodeShiftable != null && ownerShiftable != null)
-		{
-			var univPos = Universe.Current.GetUniversePosition(transform.position);
-			explodeShiftable.SetShiftPosition(univPos);
-		}
+        if (explodeShiftable != null && ownerShiftable != null)
+        {
+            var univPos = Universe.Current.GetUniversePosition(transform.position);
+            explodeShiftable.SetShiftPosition(univPos);
+        }
 
-		var count = Physics.OverlapSphereNonAlloc(transform.position, 15f, _damageColliders, _detectableMask);
-		for (var i = 0; i < count; i++)
-		{
-			_detectable = _damageColliders[i].GetComponent<Detectable>();
-			if (_detectable != null)
-			{
-				_killable = _detectable.TargetTransform.GetComponent<Killable>();
-				var damage = Mathf.Round(100f * GetDamageFraction(_detectable.transform.position, transform.position, 5f, 15f));
-				_killable.Damage(damage, transform.position, Vector3.up, Owner);
-			}
+        var count = Physics.OverlapSphereNonAlloc(transform.position, 15f, _damageColliders, _detectableMask);
+        for (var i = 0; i < count; i++)
+        {
+            _detectable = _damageColliders[i].GetComponent<Detectable>();
+            if (_detectable != null)
+            {
+                _killable = _detectable.TargetTransform.GetComponent<Killable>();
+                var damage = Mathf.Round(100f * GetDamageFraction(_detectable.transform.position, transform.position, 5f, 15f));
+                _killable.Damage(damage, transform.position, Vector3.up, Owner);
+            }
             var rBody = _damageColliders[i].GetComponentInParent<Rigidbody>();
             if (rBody != null)
                 rBody.AddExplosionForce(MissileForce, transform.position, 15f, 0f, ForceMode.Impulse);
         }
 
-		Stop();
-	}
+        Stop();
+    }
 
-	private float GetDamageFraction(Vector3 targetPosition, Vector3 damagePosition, float minDistance, float maxDistance)
-	{
-		var toDamage = targetPosition - damagePosition;
-		if (toDamage.sqrMagnitude < minDistance * minDistance)
-			return 1f;
-		if (toDamage.sqrMagnitude > maxDistance * maxDistance)
-			return 0f;
-		return 1f - Mathf.Clamp((toDamage.magnitude - minDistance) / (maxDistance - minDistance), 0, 1f);
-	}
+    private float GetDamageFraction(Vector3 targetPosition, Vector3 damagePosition, float minDistance, float maxDistance)
+    {
+        var toDamage = targetPosition - damagePosition;
+        if (toDamage.sqrMagnitude < minDistance * minDistance)
+            return 1f;
+        if (toDamage.sqrMagnitude > maxDistance * maxDistance)
+            return 0f;
+        return 1f - Mathf.Clamp((toDamage.magnitude - minDistance) / (maxDistance - minDistance), 0, 1f);
+    }
 
-	public override void Stop()
-	{
-		base.Stop();
-		_target = null;
-		Rocket.enabled = false;
-		ThrusterMesh.enabled = false;
-		Tracer.Stop();
-		Flare.SetVisible(false);
-		if (_resourcePoolItem != null)
-			_resourcePoolItem.IsAvailable = true;
-	}
+    public override void Stop()
+    {
+        base.Stop();
+        _target = null;
+        Rocket.enabled = false;
+        ThrusterMesh.enabled = false;
+        Tracer.Stop();
+        Flare.SetVisible(false);
+        if (_resourcePoolItem != null)
+            _resourcePoolItem.IsAvailable = true;
+    }
 }
