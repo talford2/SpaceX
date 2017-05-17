16355bf7e7cbbd0fc13dbb1d4ba8bd5cd2f6201f
Bodedoctor <alford.steven@gmail.com>
2017-05-17 21:44:28 +0800

    Add DataFile.

---
+++ Assets/Scripts/DataFile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+public abstract class DataFile<T>
+{
+    public static T ReadFromFile(string filename)
+    {
+        var serializer = new XmlSerializer(typeof(T));
+        Debug.Log("read: " + filename);
+        using (var fileStream = new FileStream(filename, FileMode.Open))
+        {
+            return (T)serializer.Deserialize(fileStream);
+        }
+    }
+
+    public void WriteToFile(string filename)
+    {
+        var serializer = new XmlSerializer(typeof(T));
+        using (var fileStream = new FileStream(filename, FileMode.Create))
+        {
+            serializer.Serialize(fileStream, this);
+        }
+    }
+}
