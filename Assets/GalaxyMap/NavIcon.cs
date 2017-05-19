555e06076d7be324dc807365fd0c49d9a3aacd5c
Bodedoctor <alford.steven@gmail.com>
2017-05-19 23:43:26 +0800

    Replace planets with map pins.

--- Assets/GalaxyMap/NavIcon.cs
+++ Assets/GalaxyMap/NavIcon.cs
@@ -3,10 +3,17 @@
 public class NavIcon : MonoBehaviour
 {
     public Transform DestinationPoint;
+    public Transform Pin;
+
     public int LevelIndex;
 
     public void OnClick()
     {
         NavShipController.Current.SetDestination(DestinationPoint.position);
+    }
+
+    private void Update()
+    {
+        Pin.Rotate(Vector3.up, 90f * Time.deltaTime);
     }
 }
