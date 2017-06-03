f18318f47b389d5a27690de5013cff7ace9c9d94
Bodedoctor <alford.steven@gmail.com>
2017-06-03 13:04:11 +0800

    ResetToMesh editor tweak.

--- Assets/Editor/ResetToMeshEditor.cs
+++ Assets/Editor/ResetToMeshEditor.cs
@@ -7,8 +7,10 @@ public class ResetToMeshEditor : Editor {
     public override void OnInspectorGUI()
     {
         var resetToMesh = (ResetToMesh)target;
-        base.OnInspectorGUI();
+        //base.OnInspectorGUI();
 
+        resetToMesh.Instance = (GameObject)EditorGUILayout.ObjectField("Instance", resetToMesh.Instance, typeof(GameObject), false);
+        resetToMesh.PrefabMesh = (GameObject)EditorGUILayout.ObjectField("Prefab Mesh", resetToMesh.Instance, typeof(GameObject), false);
         if (GUILayout.Button("Match Transforms"))
         {
             Utility.MatchToMesh(resetToMesh.Instance, resetToMesh.PrefabMesh);
