2bbabf6e5e27fb1a83fe7a70b6771ab7d437ae8b
Bodedoctor <alford.steven@gmail.com>
2017-06-03 23:21:38 +0800

    Display game time (debug only).

---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDisplay : MonoBehaviour
+{
+    public bool DebugBuildOnly = true;
+    private GUIStyle displayStyle;
+    
+    private float minutes;
+    private float seconds;
+
+    private void Awake()
+    {
+        if (DebugBuildOnly)
+        {
+            if (!Debug.isDebugBuild)
+                Destroy(gameObject);
+        }
+        displayStyle = new GUIStyle()
+        {
+            fontSize = 30,
+            alignment = TextAnchor.UpperRight,
+            normal = new GUIStyleState { textColor = Color.white }
+        };
+    }
+
+    private void Update()
+    {
+        minutes = Mathf.Floor(Time.time / 60f);
+        seconds = Time.time - (minutes * 60f);
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 200, 60f, 180f, 50f), string.Format("{0:f0}:{1:00}", minutes, seconds), displayStyle);
+    }
+}
