86b1f03c1677f6b5af46b2f86788a8b68d2ed5ae
Bodedoctor <alford.steven@gmail.com>
2017-06-04 18:35:53 +0800

    Separate out grand mother explosion effect.

---
+++ Assets/Vehicles/Mothership/CastExploder.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastExploder : MonoBehaviour
+{
+    public bool IsExploding = false;
+
+    public GameObject ProjectExplosionPrefab;
+    public Transform ProjectExplosionVertex1;
+    public Transform ProjectExplosionVertex2;
+    public float ProjectExplosionRadius;
+
+    private bool _isExploding;
+    private float _explodeCooldown;
+
+    private void Awake()
+    {
+        _isExploding = IsExploding;
+    }
+
+    private void Update()
+    {
+        if (_isExploding)
+        {
+            if (_explodeCooldown >= 0f)
+            {
+                _explodeCooldown -= Time.deltaTime;
+                if (_explodeCooldown < 0f)
+                {
+                    Debug.Log("TRIGGGER BOOM!");
+                    ProjectedExplosion();
+                    _explodeCooldown = Random.Range(0.005f, 0.01f);
+                }
+            }
+        }
+    }
+
+    private void ProjectedExplosion()
+    {
+        var castFrom = transform.position + Random.onUnitSphere * ProjectExplosionRadius;
+        var castDelta = ProjectExplosionVertex2.position - ProjectExplosionVertex1.position;
+        var castTo = ProjectExplosionVertex1.position + Random.value * castDelta.magnitude * castDelta.normalized;
+        var cast = new Ray(castFrom, castTo - castFrom);
+        RaycastHit castHit;
+
+        Debug.DrawLine(castFrom, castTo, Color.yellow, 1f);
+        if (Physics.Raycast(cast, out castHit, ProjectExplosionRadius, LayerMask.GetMask("ExplodeCast", "Distant")))
+        {
+            Debug.Log("EXPLODE AT: " + castHit.point);
+            var explosion = ResourcePoolManager.GetAvailable(ProjectExplosionPrefab, castHit.point - castHit.normal, Quaternion.identity);
+            var shakeSource = explosion.GetComponent<ScreenShakeSource>();
+            if (shakeSource != null)
+                shakeSource.Trigger();
+            SplashDamage.ExplodeAt(castHit.point, 100f, 40f, 200f, 200f, LayerMask.GetMask("Detectable"), null);
+        }
+        else
+        {
+            Debug.Log("NO CAST HIT");
+        }
+    }
+
+    public void Trigger()
+    {
+        _isExploding = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ProjectExplosionRadius);
+    }
+}
--- Assets/Vehicles/Mothership/GrandMothership.cs
+++ Assets/Vehicles/Mothership/GrandMothership.cs
@@ -30,20 +30,12 @@ public class GrandMothership : MonoBehaviour
     public GameObject SoundPrefab;
     public AudioClip DefeatedSound;
 
-    [Header("Destroy Explosion")]
-    public bool IsExploding = false;
-    public GameObject ProjectExplosionPrefab;
-    public Transform ProjectExplosionVertex1;
-    public Transform ProjectExplosionVertex2;
-    public float ProjectExplosionRadius;
+    [Header("Destruction")]
+    public CastExploder BoomCaster;
 
     private int _liveCount;
     private List<Killable> _killables;
     private List<ProximitySpawner> _spawners;
-
-    // Exploding
-    private bool _isExploding;
-    private float _explodeCooldown;
 
     private void Awake()
     {
@@ -95,40 +87,6 @@ public class GrandMothership : MonoBehaviour
         {
             killable.OnDie += OnKill;
         }
-        _isExploding = IsExploding;
-    }
-
-    private void ProjectedExplosion()
-    {
-        var castFrom = Random.onUnitSphere * ProjectExplosionRadius;
-        var castDelta = ProjectExplosionVertex2.position - ProjectExplosionVertex1.position;
-        var castTo = ProjectExplosionVertex1.position + Random.value * castDelta.magnitude * castDelta.normalized;
-        var cast = new Ray(castFrom, castTo - castFrom);
-        RaycastHit castHit;
-        if (Physics.Raycast(cast, out castHit, ProjectExplosionRadius, LayerMask.GetMask("ExplodeCast")))
-        {
-            var explosion = ResourcePoolManager.GetAvailable(ProjectExplosionPrefab, castHit.point - castHit.normal, Quaternion.identity);
-            var shakeSource = explosion.GetComponent<ScreenShakeSource>();
-            if (shakeSource != null)
-                shakeSource.Trigger();
-            SplashDamage.ExplodeAt(castHit.point, 100f, 40f, 200f, 200f, LayerMask.GetMask("Detectable"), null);
-        }
-    }
-
-    private void Update()
-    {
-        if (_isExploding)
-        {
-            if (_explodeCooldown >= 0f)
-            {
-                _explodeCooldown -= Time.deltaTime;
-                if (_explodeCooldown < 0f)
-                {
-                    ProjectedExplosion();
-                    _explodeCooldown = Random.Range(0.005f, 0.01f);
-                }
-            }
-        }
     }
 
     private void OnKill(Killable sender, GameObject attacker)
@@ -150,14 +108,8 @@ public class GrandMothership : MonoBehaviour
                     .GetComponent<AnonymousSound>()
                     .PlayAt(DefeatedSound, Universe.Current.ViewPort.transform.position, 1f, false);
             }
-            _isExploding = true;
             MapPin.SetPinState(MapPin.MapPinState.Inactive);
+            BoomCaster.Trigger();
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, ProjectExplosionRadius);
     }
 }
