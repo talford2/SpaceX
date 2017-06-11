0a23359aa3bed470bdf43d0274d0eb7a7ccbb130
Bodedoctor <alford.steven@gmail.com>
2017-06-12 01:48:41 +0800

    Display primary and secondary weapon names in hangar.

--- Assets/Hangar/HangarScreen.cs
+++ Assets/Hangar/HangarScreen.cs
@@ -5,20 +5,32 @@ using UnityEngine.UI;
 public class HangarScreen : MonoBehaviour
 {
     public Text CreditText;
+    public Text PrimaryWeaponText;
+    public Text SecondaryWeaponText;
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+    }
 
+    private void Start()
+    {
         var playerFile = PlayerFile.ReadFromFile(PlayerFile.Filename);
         UpdateCredits(playerFile.SpaceJunk);
+        UpdateLeftBar(WeaponDefinitionPool.ByKey(playerFile.PrimaryWeaponKey), WeaponDefinitionPool.ByKey(playerFile.SecondaryWeaponKey));
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
             SceneManager.LoadScene("Menu");
+    }
+
+    private void UpdateLeftBar(WeaponDefinition primaryWeapon, WeaponDefinition secondaryWeapon)
+    {
+        PrimaryWeaponText.text = primaryWeapon.Name;
+        SecondaryWeaponText.text = secondaryWeapon.Name;
     }
 
     private void UpdateCredits(int creditCount)
