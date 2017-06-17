631a44f9a38678eee49fc8e2ccbb13c6168d5019
Bodedoctor <alford.steven@gmail.com>
2017-06-17 21:04:25 +0800

    Create properties for mission complete sounds.

--- Assets/GUI/MissionCompleteScreen.cs
+++ Assets/GUI/MissionCompleteScreen.cs
@@ -11,6 +11,10 @@ public class MissionCompleteScreen : MonoBehaviour
     public Text EarnedCreditsText;
     public Text TotalCreditsText;
 
+    public AudioClip TriggerSound;
+    public AudioClip TextAppearSound;
+    public AudioClip CreditTickSound;
+
     private static MissionCompleteScreen _current;
 
     public static MissionCompleteScreen Current { get { return _current; } }
@@ -21,27 +25,42 @@ public class MissionCompleteScreen : MonoBehaviour
         Hide();
     }
 
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            var viewPort = Universe.Current.ViewPort.transform;
+            ResourcePoolManager
+                .GetAvailable(ResourcePoolIndex.AnonymousSound, viewPort.position, Quaternion.identity)
+                .GetComponent<AnonymousSound>().PlayAt(clip, viewPort.position, 1f, false);
+        }
+    }
+
     public void Show(int playerKills)
     {
         ScreenGroup.alpha = 1f;
+        PlaySound(TriggerSound);
         var delay = 0.5f;
         StartCoroutine(FadeIn(delay));
-        delay += 0.5f;
+        delay += 1f;
         StartCoroutine(DelayedAction(delay, () => {
             KillsText.enabled = true;
             KillsText.text = string.Format("Kills: {0:N0}", playerKills);
+            PlaySound(TextAppearSound);
         }));
         delay += 0.5f;
         StartCoroutine(DelayedAction(delay, () =>
         {
             EarnedCreditsText.enabled = true;
             EarnedCreditsText.text = string.Format("Credits Earned: {0:N0}", Mission.Current.GetEarnedCredits());
+            PlaySound(TextAppearSound);
         }));
         delay += 0.5f;
         StartCoroutine(DelayedAction(delay, () =>
         {
             TotalCreditsText.enabled = true;
             TotalCreditsText.text = string.Format("TOTAL CREDITS: {0:N0}", PlayerController.Current.SpaceJunkCount - Mission.Current.GetEarnedCredits());
+            PlaySound(TextAppearSound);
         }));
         delay += 1f;
         StartCoroutine(DelayedAction(delay, () =>
@@ -92,6 +111,7 @@ public class MissionCompleteScreen : MonoBehaviour
             totalCredits++;
             EarnedCreditsText.text = string.Format("Credits Earned: {0:N0}", credits);
             TotalCreditsText.text = string.Format("TOTAL CREDITS: {0:N0}", totalCredits);
+            PlaySound(CreditTickSound);
         }
     }
 }
