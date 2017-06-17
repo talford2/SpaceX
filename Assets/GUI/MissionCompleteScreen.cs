f64734285801379d1dccfe96fd78a4e2ed6399b6
Bodedoctor <alford.steven@gmail.com>
2017-06-17 13:47:45 +0800

    Complete screen progress.

--- Assets/GUI/MissionCompleteScreen.cs
+++ Assets/GUI/MissionCompleteScreen.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MissionCompleteScreen : MonoBehaviour
 {
     public CanvasGroup ScreenGroup;
+    public Text TimeText;
+    public Text KillsText;
 
     private static MissionCompleteScreen _current;
 
@@ -17,10 +22,20 @@ public class MissionCompleteScreen : MonoBehaviour
     public void Show()
     {
         ScreenGroup.alpha = 1f;
+        StartCoroutine(DelayedAction(0.5f, () => { TimeText.enabled = true; }));
+        StartCoroutine(DelayedAction(1f, () => { KillsText.enabled = true; }));
     }
 
     public void Hide()
     {
         ScreenGroup.alpha = 0;
+        TimeText.enabled = false;
+        KillsText.enabled = false;
+    }
+
+    private IEnumerator DelayedAction(float delay, Action action)
+    {
+        yield return new WaitForSeconds(delay);
+        action();
     }
 }
