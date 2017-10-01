2fedb7a4c436b80ea1501043e0d56194887092ed
Bodedoctor <alford.steven@gmail.com>
2017-10-01 15:54:16 +0800

    Tweaking scenario 1.

---
+++ Assets/Environment/Asteroid/Asteroid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public GameObject DieEffectPrefab;
+
+    private Killable _killable;
+    private GameObject _effectInstance;
+
+    private void Awake()
+    {
+        _killable = GetComponent<Killable>();
+        _killable.OnDie += OnDie;
+    }
+
+    private void OnDie(Killable sender, Vector3 position, Vector3 normal, GameObject attacker)
+    {
+        Debug.Log("DESTROYASTEROID!");
+        PlaceEffect(transform.position, normal, null);
+        Destroy(gameObject);
+    }
+
+    public void PlaceEffect(Vector3 position, Vector3 normal, Transform parent)
+    {
+        if (DieEffectPrefab != null)
+        {
+            _effectInstance = ResourcePoolManager.GetAvailable(DieEffectPrefab, position, transform.rotation);
+
+            var hitEffectShiftable = _effectInstance.GetComponent<Shiftable>();
+            if (hitEffectShiftable != null)
+            {
+                hitEffectShiftable.SetShiftPosition(Universe.Current.GetUniversePosition(position));
+            }
+
+            _effectInstance.transform.position = position;
+            _effectInstance.transform.forward = normal;
+        }
+    }
+}
