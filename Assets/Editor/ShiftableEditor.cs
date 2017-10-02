80f2564fdbefc7e5fadcdbafa1e084dc2168f081
Bodedoctor <alford.steven@gmail.com>
2017-10-02 16:32:06 +0800

    Improving shiftable editor.

--- Assets/Editor/ShiftableEditor.cs
+++ Assets/Editor/ShiftableEditor.cs
@@ -7,12 +7,38 @@ public class ShiftableEditor : Editor
     public override void OnInspectorGUI()
     {
         var shiftable = (Shiftable)target;
-        base.OnInspectorGUI();
+
+        shiftable.UseWorldPosition = EditorGUILayout.Toggle("Use World Position", shiftable.UseWorldPosition);
+
+        EditorGUI.BeginDisabledGroup(shiftable.UseWorldPosition);
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.PrefixLabel("Universe Cell Index");
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("X");
+        shiftable.UniverseCellIndex.X = EditorGUILayout.IntField(shiftable.UniverseCellIndex.X);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Y");
+        shiftable.UniverseCellIndex.Y = EditorGUILayout.IntField(shiftable.UniverseCellIndex.Y);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Z");
+        shiftable.UniverseCellIndex.Z = EditorGUILayout.IntField(shiftable.UniverseCellIndex.Z);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndHorizontal();
+
+        shiftable.CellLocalPosition = EditorGUILayout.Vector3Field("Cell Local Position", shiftable.CellLocalPosition);
 
         if (GUILayout.Button("Calculate UniversePosition"))
         {
             var universePosition = Universe.GetUniversePositionFromZero(shiftable.transform.position, 1000f);
             shiftable.SetShiftPosition(universePosition);
         }
+        EditorGUI.EndDisabledGroup();
     }
 }
