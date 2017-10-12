0fca8d237fbbe5442c902bde7eedd0de5b79d935
Bodedoctor <alford.steven@gmail.com>
2017-10-12 18:38:18 +0800

    Delay spawning from enemy mother ship.

---
+++ Assets/Scripts/DelayedTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayedTrigger : Triggerable
+{
+    public bool StartOn = true;
+    public float Delay;
+    public Triggerable TriggerObject;
+
+    private void Start()
+    {
+        if (StartOn)
+            Trigger();
+    }
+
+    public override void Trigger(float delay = 0)
+    {
+        StartCoroutine(DelayTrigger(Delay, TriggerObject));
+    }
+
+    private IEnumerator DelayTrigger(float delay, Triggerable triggerable)
+    {
+        yield return new WaitForSeconds(delay);
+        triggerable.Trigger();
+    }
+}
