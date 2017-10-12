ca87ce11eeeac69e0f67cddaddcdaacd659ed260
Bodedoctor <alford.steven@gmail.com>
2017-10-12 20:28:55 +0800

    Assigning kill objectives to flak turrets.

---
+++ Assets/Environment/MultipleKillTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleKillTrigger : MonoBehaviour
+{
+    public List<Killable> Killables;
+    public Triggerable OnAllKilledTrigger;
+    public VehicleTrackerValues TrackerValues;
+
+    private int _triggerDieCount;
+    private int _dieCount;
+
+    public void Initialize()
+    {
+        foreach(var killable in Killables)
+        {
+            var tracker = killable.gameObject.GetComponentInChildren<TurretTracker>();
+            TrackerManager.Current.RemoveTracker(tracker);
+            tracker.DestroyInstance();
+            //TrackerManager.Current.AddTracker(tracker);
+
+            var objectiveTracker = tracker.gameObject.AddComponent<VehicleTracker>();
+            objectiveTracker.Options = TrackerValues;
+
+
+            /*
+            if (TrackerValues != null)
+            {
+                var objectiveTracker = killable.gameObject.AddComponent<VehicleTracker>();
+                objectiveTracker.Options = TrackerValues;
+
+                if (killable.gameObject.GetComponentInChildren<Targetable>() == null)
+                    Debug.LogWarning("NO TRACKER FOUND!");
+                objectiveTracker.Targetable = killable.gameObject.GetComponentInChildren<Targetable>();
+                objectiveTracker.Killable = killable;
+            }
+            */
+            killable.OnDie += OnKillableDie;
+        }
+        _dieCount = 0;
+        _triggerDieCount = Killables.Count;
+    }
+
+    private void OnKillableDie(Killable sender, Vector3 position, Vector3 normal, GameObject attacker)
+    {
+        _dieCount++;
+        sender.OnDie -= OnKillableDie;
+        if (_dieCount == _triggerDieCount)
+        {
+            Debug.Log("TRIGGER SOMETHING");
+            OnAllKilledTrigger.Trigger();
+        }
+    }
+}
--- Assets/Vehicles/Mothership/GrandMothership.cs
+++ Assets/Vehicles/Mothership/GrandMothership.cs
@@ -8,10 +8,12 @@ public class GrandMothership : MonoBehaviour
     public GameObject LaserTurretPrefab;
     public Transform LaserTurretGroupTransform;
     //public List<Transform> LaserTurretTransforms;
+
     [Header("Flak Turrets")]
     public GameObject FlakTurretPrefab;
     public Transform FlakTurretGroupTransform;
     //public List<Transform> FlakTurretTransforms;
+    public MultipleKillTrigger FlakTurretKillTrigger;
 
     [Header("Tanks")]
     public GameObject TankPrefab;
@@ -57,6 +59,7 @@ public class GrandMothership : MonoBehaviour
             turret.transform.SetParent(turretTransform);
             turret.transform.up = turretTransform.forward;
             //_killables.Add(turret.GetComponent<Killable>());
+            FlakTurretKillTrigger.Killables.Add(turret.GetComponent<Killable>());
         }
         foreach(Transform tankTransform in TankGroupTransform)
         {
@@ -93,6 +96,12 @@ public class GrandMothership : MonoBehaviour
             ResourcePoolManager.GetAvailable(SuperNovaPrefab, transform.position, Quaternion.identity);
             Destroy(universeEvent.gameObject);
         };
+    }
+
+    private void Start()
+    {
+        FlakTurretKillTrigger.Initialize();
+
     }
 
     private void OnKill(Killable sender, Vector3 positon, Vector3 normal, GameObject attacker)
