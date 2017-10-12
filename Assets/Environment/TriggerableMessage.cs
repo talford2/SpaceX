289d13698e9e1e04197b1d8e177b55922af030d2
Bodedoctor <alford.steven@gmail.com>
2017-10-12 21:10:49 +0800

    Commit missed code.

---
+++ Assets/Environment/TriggerableMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TriggerableMessage : Triggerable
+{
+    public float Delay;
+    public string Author;
+    [Multiline]
+    public string Message;
+
+    public override void Trigger(float delay = 0)
+    {
+        StartCoroutine(DisplayMessage(Delay));
+    }
+
+    private IEnumerator DisplayMessage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        CommMessaging.Current.ShowMessage(Player.Current.VehicleInstance.gameObject, Author, Message);
+    }
+}
