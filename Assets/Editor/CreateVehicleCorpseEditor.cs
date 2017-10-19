e6c0920fb007e2d04a44c1e9149e8e381fa83501
Bodedoctor <alford.steven@gmail.com>
2017-10-19 11:20:04 +0800

    Improve create vehicle corpse editor.

--- Assets/Editor/CreateVehicleCorpseEditor.cs
+++ Assets/Editor/CreateVehicleCorpseEditor.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class CreateVehicleCorpseEditor : Editor
+public class CreateVehicleCorpseEditor : EditorWindow
 {
+    private string renameTo;
+    private GameObject explosionPrefab;
+    private GameObject smokePrefab;
+    private static EditorWindow windowInstance;
+
     [MenuItem("Space X/Build Corpse")]
-    public static void BuildCorpse()
+    public static void ShowWindow()
+    {
+        windowInstance = GetWindow(typeof(CreateVehicleCorpseEditor));
+    }
+
+    public void BuildCorpse()
     {
         var selected = Selection.activeGameObject;
         if (selected != null)
         {
+            selected.name = renameTo;
+
             var rigidBody = selected.AddComponent<Rigidbody>();
             rigidBody.mass = 400f;
             rigidBody.useGravity = false;
@@ -19,7 +31,53 @@ public class CreateVehicleCorpseEditor : Editor
             killable.DestroyOnDie = false;
 
             selected.AddComponent<Shiftable>();
-            selected.AddComponent<VehicleCorpse>();
+            var corpse = selected.AddComponent<VehicleCorpse>();
+            corpse.ExplosionPrefab = explosionPrefab;
+
+            foreach (Transform transform in selected.GetComponentsInChildren<Transform>())
+            {
+                var transName = transform.name.ToLowerInvariant();
+                if (transName.Contains("thruster"))
+                {
+                    var smokeInstance = Instantiate(smokePrefab, transform);
+                    smokeInstance.transform.localPosition = Vector3.zero;
+                    smokeInstance.transform.localRotation = Quaternion.identity;
+                    smokeInstance.transform.localScale = Vector3.one;
+                }
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        var selected = Selection.activeGameObject;
+        if (selected)
+        {
+            EditorGUILayout.PrefixLabel("Selected");
+            var boldText = new GUIStyle();
+            boldText.fontStyle = FontStyle.Bold;
+            EditorGUILayout.LabelField(selected.name, boldText);
+            renameTo = string.Format("{0}Corpse", selected.name);
+
+            EditorGUILayout.PrefixLabel("Rename to");
+            renameTo = EditorGUILayout.TextField(renameTo);
+
+            EditorGUILayout.PrefixLabel("Explosion Prefab");
+            explosionPrefab = (GameObject)EditorGUILayout.ObjectField(explosionPrefab, typeof(GameObject), false);
+            EditorGUILayout.PrefixLabel("Smoke Prefab");
+            smokePrefab = (GameObject)EditorGUILayout.ObjectField(smokePrefab, typeof(GameObject), false);
+            if (!string.IsNullOrEmpty(renameTo) && explosionPrefab != null && smokePrefab != null)
+            {
+                if (GUILayout.Button("Create"))
+                {
+                    BuildCorpse();
+                    windowInstance.Close();
+                }
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Select an object to transform in to corpse.");
         }
     }
 }
